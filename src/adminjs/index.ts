9c479c3cf83cc2697de284846642cda48bc5c734
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2022-09-08 16:34:59 -0300

    Criando a interface do adminjs e configurando ele

---
+++ src/adminjs/index.ts
@@ -0,0 +1,34 @@
+import AdminJs from "adminjs";
+import AdminJsExpress from "@adminjs/express";
+import AdminJsSequelize from "@adminjs/sequelize";
+import { sequelize } from "../database";
+
+AdminJs.registerAdapter(AdminJsSequelize);
+
+export const adminJs = new AdminJs({
+  databases: [sequelize],
+  rootPath: "/admin",
+  branding: {
+    companyName: "Animeflix",
+    logo: "/animeflix-trasnparent.png",
+    theme: {
+      colors: {
+        primary100: "#ff0043",
+        primary80: "#ff1a57",
+        primary60: "#ff3369",
+        primary40: "#ff4d7c",
+        primary20: "#ff668f",
+        grey100: "#151515",
+        grey80: "#333333",
+        grey60: "#4d4d4d",
+        grey40: "#666666",
+        grey20: "#dddddd",
+        filterBg: "#333333",
+        accent: "#151515",
+        hoverBg: "#151515",
+      },
+    },
+  },
+});
+
+export const adminJsRouter = AdminJsExpress.buildRouter(adminJs);
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
 import { sequelize } from './database/index';
 import express from "express"
+import {adminJs, adminJsRouter} from "./adminjs"
 import * as dotenv from "dotenv";
 
 const app = express()
 dotenv.config();
+
+app.use(express.static('public'))
+app.use(adminJs.options.rootPath, adminJsRouter)
 
 const PORT = process.env.PORT || 3000
 
