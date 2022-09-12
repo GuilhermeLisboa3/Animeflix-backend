bc4ddaa09d8de7df68fbba0716cc0cd3616366c9
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2022-09-12 14:52:10 -0300

    Incluindo a primeira rota, e fazendo a rota para mostrar todas as  categorias

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,14 @@ import { sequelize } from './database/index';
 import express from "express"
 import {adminJs, adminJsRouter} from "./adminjs"
 import * as dotenv from "dotenv";
+import router from './routes';
 
 const app = express()
 dotenv.config();
 
 app.use(express.static('public'))
 app.use(adminJs.options.rootPath, adminJsRouter)
+app.use(router)
 
 const PORT = process.env.PORT || 3000
 
