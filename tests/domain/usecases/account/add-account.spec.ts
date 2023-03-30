74a60f202b79b4f16054339a5f27fcb512f4889b
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-03-30 13:06:40 -0300

    test: ensure AddAccountUseCase call HashGenerator with correct input

--- tests/domain/usecases/account/add-account.spec.ts
+++ tests/domain/usecases/account/add-account.spec.ts
@@ -1,22 +1,25 @@
 import { AddAccount, addAccountUseCase } from '@/domain/usecases/account'
 import { CheckAccountByEmail } from '@/domain/contracts/database/account'
+import { HashGenerator } from '@/domain/contracts/gateways'
 import { FieldInUseError } from '@/domain/errors'
 
 import { mock, MockProxy } from 'jest-mock-extended'
 
 describe('AddAccount', () => {
   let accountRepository: MockProxy<CheckAccountByEmail>
+  let hash: MockProxy<HashGenerator>
   let sut: AddAccount
   let account: { email: string, password: string, firstName: string, lastName: string, phone: string, birth: Date }
 
   beforeAll(() => {
     accountRepository = mock()
     accountRepository.checkByEmail.mockResolvedValue(false)
+    hash = mock()
     account = { email: 'any_email', password: 'any_password', firstName: 'any_first_name', lastName: 'any_last_name', phone: 'any_phone', birth: new Date() }
   })
 
   beforeEach(() => {
-    sut = addAccountUseCase(accountRepository)
+    sut = addAccountUseCase(accountRepository, hash)
   })
 
   it('should call CheckAccountByEmail with correct email', async () => {
@@ -32,5 +35,12 @@ describe('AddAccount', () => {
     const promise = sut(account)
 
     await expect(promise).rejects.toThrow(new FieldInUseError('email'))
+  })
+
+  it('should call HashGenerator with correct input', async () => {
+    await sut(account)
+
+    expect(hash.generate).toHaveBeenCalledWith({ plaintext: 'any_password' })
+    expect(hash.generate).toHaveBeenCalledTimes(1)
   })
 })
