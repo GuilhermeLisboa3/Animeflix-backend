4fbcdba852760a02d0e4b477a5b7a19a7303758b
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-03-30 17:15:27 -0300

    feat: ensure JwtAdapter return id on success

--- src/infra/gateways/jwt-adapter.ts
+++ src/infra/gateways/jwt-adapter.ts
@@ -2,14 +2,15 @@ import { TokenGenerator, TokenValidator } from '@/domain/contracts/gateways'
 
 import jwt from 'jsonwebtoken'
 
-export class JwtAdapter implements TokenGenerator {
+export class JwtAdapter implements TokenGenerator, TokenValidator {
   constructor (private readonly secret: string) {}
 
   async generate ({ key }: TokenGenerator.Input): Promise<TokenGenerator.Output> {
     return jwt.sign({ key }, this.secret, { expiresIn: '1d' })
   }
 
-  async validate ({ token }: TokenValidator.Input): Promise<void> {
-    jwt.verify(token, this.secret) as any
+  async validate ({ token }: TokenValidator.Input): Promise<TokenValidator.Output> {
+    const { key } = jwt.verify(token, this.secret) as any
+    return key
   }
 }
