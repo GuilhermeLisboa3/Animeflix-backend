b2623a9bdacabb3ea6bc2c6b15c9e7b3e782af39
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-03-30 13:44:50 -0300

    test: ensure EmailValidator return InvalidFieldError if email is invalid

---
+++ tests/application/validation/email-validator.spec.ts
@@ -0,0 +1,12 @@
+import { InvalidFieldError } from '@/application/errors'
+import { EmailValidator } from '@/application/validation'
+
+describe('EmailValidator', () => {
+  it('should return InvalidFieldError if email is invalid', () => {
+    const sut = new EmailValidator('invalid_email', 'email')
+
+    const error = sut.validate()
+
+    expect(error).toEqual(new InvalidFieldError('email'))
+  })
+})
