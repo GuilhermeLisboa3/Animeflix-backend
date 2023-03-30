282d5c8a4ffdf4169bf1879dd8430a7641b341a8
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-03-30 17:04:57 -0300

    test: ensure AuthorizeUseCase call CheckAccountRole with correct input

--- tests/domain/usecases/account/authorize.spec.ts
+++ tests/domain/usecases/account/authorize.spec.ts
@@ -1,21 +1,25 @@
 import { Authorize, AuthorizeUseCase } from '@/domain/usecases/account'
 import { TokenValidator } from '@/domain/contracts/gateways'
+import { CheckAccountRole } from '@/domain/contracts/database/account'
 
 import { mock, MockProxy } from 'jest-mock-extended'
 import { AuthenticationError } from '@/domain/errors'
 
 describe('AuthorizeUseCase', () => {
   let token: MockProxy<TokenValidator>
+  let accountRepository: MockProxy<CheckAccountRole>
   let sut: Authorize
   let makeParams: { accessToken: string, role?: string }
 
   beforeAll(() => {
     token = mock()
+    token.validate.mockResolvedValue('any_id')
+    accountRepository = mock()
     makeParams = { accessToken: 'any_token', role: 'any_role' }
   })
 
   beforeEach(() => {
-    sut = AuthorizeUseCase(token)
+    sut = AuthorizeUseCase(token, accountRepository)
   })
 
   it('should call TokenValidator with correct input', async () => {
@@ -31,5 +35,12 @@ describe('AuthorizeUseCase', () => {
     const promise = sut(makeParams)
 
     await expect(promise).rejects.toThrow(new AuthenticationError())
+  })
+
+  it('should call CheckAccountRole with correct input', async () => {
+    await sut(makeParams)
+
+    expect(accountRepository.checkRole).toHaveBeenCalledWith({ accountId: 'any_id', role: 'any_role' })
+    expect(accountRepository.checkRole).toHaveBeenCalledTimes(1)
   })
 })
