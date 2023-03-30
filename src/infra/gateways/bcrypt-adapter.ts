5daf156173dc95b1d615c2c8cc0ddcdca92dd765
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-03-30 15:48:59 -0300

    feat: ensure BcryptAdapter return false if the values are different

--- src/infra/gateways/bcrypt-adapter.ts
+++ src/infra/gateways/bcrypt-adapter.ts
@@ -2,14 +2,14 @@ import { HashComparer, HashGenerator } from '@/domain/contracts/gateways'
 
 import bcrypt from 'bcrypt'
 
-export class BcryptAdapter implements HashGenerator {
+export class BcryptAdapter implements HashGenerator, HashComparer {
   constructor (private readonly salt: number) {}
 
   async generate ({ plaintext }: HashGenerator.Input): Promise<HashGenerator.Output> {
     return await bcrypt.hash(plaintext, this.salt)
   }
 
-  async comparer ({ plaintext, digest }: HashComparer.Input): Promise<void> {
-    await bcrypt.compare(plaintext, digest)
+  async comparer ({ plaintext, digest }: HashComparer.Input): Promise<HashComparer.Output> {
+    return await bcrypt.compare(plaintext, digest)
   }
 }
