5a5e1b232b3db04542048b38c2d72cea55d63b97
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-03-30 16:09:14 -0300

    feat: ensure LoginController return 401 if Authentication returns AuthenticationError

--- src/application/controllers/controller.ts
+++ src/application/controllers/controller.ts
@@ -1,6 +1,6 @@
-import { badRequest, HttpResponse, serverError } from '@/application/helpers'
+import { badRequest, HttpResponse, serverError, unauthorized } from '@/application/helpers'
 import { ValidationComposite, Validator } from '@/application/validation'
-import { FieldInUseError } from '@/domain/errors'
+import { AuthenticationError, FieldInUseError } from '@/domain/errors'
 
 export abstract class Controller {
   abstract perform (httpRequest: any): Promise<HttpResponse>
@@ -13,6 +13,7 @@ export abstract class Controller {
       return await this.perform(httpRequest)
     } catch (error) {
       if (error instanceof FieldInUseError) return badRequest(error)
+      if (error instanceof AuthenticationError) return unauthorized()
       return serverError(error)
     }
   }
--- src/application/helpers/http.ts
+++ src/application/helpers/http.ts
@@ -1,4 +1,4 @@
-import { ServerError } from '@/application/errors/http'
+import { ServerError, UnauthorizedError } from '@/application/errors/http'
 
 export interface HttpResponse<T = any> {
   statusCode: number
@@ -13,6 +13,11 @@ export const ok = <T = any >(data: T): HttpResponse<T> => ({
 export const badRequest = (error: Error): HttpResponse<Error> => ({
   statusCode: 400,
   data: error
+})
+
+export const unauthorized = (): HttpResponse<Error> => ({
+  statusCode: 401,
+  data: new UnauthorizedError()
 })
 
 export const serverError = (error: unknown): HttpResponse<Error> => ({
