fc4b85e1a23d15ae9f14dafa2bcef163833e3ead
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-03-30 18:06:03 -0300

    test: ensure AuthAdminMiddleware return 403 if authorize fails

--- tests/main/middlewares/auth-admin.spec.ts
+++ tests/main/middlewares/auth-admin.spec.ts
@@ -1,8 +1,10 @@
-import { UnauthorizedError } from '@/application/errors'
-import { sequelize } from '@/infra/database/postgres/entities'
+import { ForbiddenError, UnauthorizedError } from '@/application/errors'
+import { Account, sequelize } from '@/infra/database/postgres/entities'
 import { app } from '@/main/config/app'
 import { authAdmin } from '@/main/middlewares'
+import env from '@/main/config/env'
 
+import { sign } from 'jsonwebtoken'
 import request from 'supertest'
 
 describe('AuthMiddleware', () => {
@@ -20,5 +22,25 @@ describe('AuthMiddleware', () => {
 
     expect(status).toBe(401)
     expect(body.error).toEqual(new UnauthorizedError().message)
+  })
+
+  it('should return 403 if authorize fails', async () => {
+    await Account.create({
+      firstName: 'any_name',
+      lastName: 'any_last_name',
+      email: 'any_email@gmail.com',
+      password: 'any_password',
+      birth: new Date(),
+      phone: 'any_phone',
+      role: 'user'
+    })
+    const token = sign({ key: '1' }, env.secret)
+
+    app.get('/fake_route', authAdmin)
+
+    const { status, body } = await request(app).get('/fake_route').set({ authorization: `Bearer: ${token}` })
+
+    expect(status).toBe(403)
+    expect(body.error).toEqual(new ForbiddenError().message)
   })
 })
