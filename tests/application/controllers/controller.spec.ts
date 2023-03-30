165cd2622acf53cfa3804427dba929e0a0dd7aa6
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-03-30 13:58:33 -0300

    test: ensure Controller return 500 if perform throws

--- tests/application/controllers/controller.spec.ts
+++ tests/application/controllers/controller.spec.ts
@@ -1,6 +1,7 @@
 import { Controller } from '@/application/controllers'
 import { HttpResponse } from '@/application/helpers'
 import { ValidationComposite } from '@/application/validation'
+import { ServerError } from '@/application/errors/http'
 
 jest.mock('@/application/validation/composite')
 
@@ -29,6 +30,18 @@ describe('Controller', () => {
     expect(httpResponse).toEqual({
       statusCode: 400,
       data: new Error('validation_error')
+    })
+  })
+
+  it('should return 500 if perform throws', async () => {
+    const error = new Error('perform_error')
+    jest.spyOn(sut, 'perform').mockRejectedValueOnce(error)
+
+    const httpResponse = await sut.handle('any_value')
+
+    expect(httpResponse).toEqual({
+      statusCode: 500,
+      data: new ServerError(error)
     })
   })
 })
