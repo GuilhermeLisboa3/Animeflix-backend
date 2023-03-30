2f443c78f8f2e12f092bb1c01bf445a57793d6b0
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-03-30 15:21:32 -0300

    test: ensure route post /auth/register return 400 if email already exists

---
+++ tests/main/routes/account.test.ts
@@ -0,0 +1,46 @@
+import { app } from '@/main/config/app'
+import { sequelize, Account } from '@/infra/database/postgres/entities'
+import { FieldInUseError } from '@/domain/errors'
+
+import request from 'supertest'
+
+describe('AccountRoute', () => {
+  let makeAccount: { firstName: string, lastName: string, email: string, password: string, birth: Date, phone: string, role: 'user' | 'admin' }
+
+  beforeAll(async () => {
+    makeAccount = {
+      firstName: 'any_name',
+      lastName: 'any_last_name',
+      email: 'any_email@gmail.com',
+      password: 'any_password',
+      birth: new Date(),
+      phone: 'any_phone',
+      role: 'user'
+    }
+  })
+  beforeEach(async () => {
+    await sequelize.sync({ force: true })
+  })
+  afterAll(async () => {
+    await sequelize.close()
+  })
+
+  describe('POST /auth/register', () => {
+    it('should return 400 if email already exists', async () => {
+      await Account.create(makeAccount)
+      const { status, body: { error } } = await request(app)
+        .post('/auth/register')
+        .send({
+          firstName: 'any_name',
+          lastName: 'any_last_name',
+          email: 'any_email@gmail.com',
+          password: 'any_password',
+          birth: new Date(),
+          phone: 'any_phone'
+        })
+
+      expect(status).toBe(400)
+      expect(error).toBe(new FieldInUseError('email').message)
+    })
+  })
+})
