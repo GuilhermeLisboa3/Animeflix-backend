6c037ce544e205641b409ac4550d47dc7c581477
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-03-30 14:11:05 -0300

    feat: ensure SignUpController extends Controller

--- src/application/controllers/account/signup.ts
+++ src/application/controllers/account/signup.ts
@@ -1,11 +1,12 @@
+import { Controller } from '@/application/controllers'
 import { HttpResponse, ok } from '@/application/helpers'
 import { Validator, ValidationBuilder as builder } from '@/application/validation'
 import { AddAccount } from '@/domain/usecases/account'
 
 type HttpRequest = { firstName: string, lastName: string, phone: string, birth: Date, email: string, password: string }
 
-export class SignUpController {
-  constructor (private readonly AddAccount: AddAccount) { }
+export class SignUpController extends Controller {
+  constructor (private readonly AddAccount: AddAccount) { super() }
 
   async perform (input: HttpRequest): Promise<HttpResponse> {
     const account = await this.AddAccount(input)
