eb124d6bdcc850c221290bf9d8f1dce6de41bb49
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-03-30 15:41:35 -0300

    test: ensure AuthenticationUS return AuthenticationError if hashComparer returns false

--- src/domain/usecases/account/authentication.ts
+++ src/domain/usecases/account/authentication.ts
@@ -1,14 +1,15 @@
 import { LoadAccountByEmail } from '@/domain/contracts/database/account'
-import { HashComparer } from '@/domain/contracts/gateways'
+import { HashComparer, TokenGenerator } from '@/domain/contracts/gateways'
 import { AuthenticationError } from '@/domain/errors'
 
-type Setup = (accountRepository: LoadAccountByEmail, hashCompare: HashComparer) => Authentication
+type Setup = (accountRepository: LoadAccountByEmail, hashCompare: HashComparer, token: TokenGenerator) => Authentication
 type Input = { email: string, password: string }
 export type Authentication = (input: Input) => Promise<void>
 
-export const AuthenticationUseCase: Setup = (accountRepository, hashCompare) => async ({ email, password }) => {
+export const AuthenticationUseCase: Setup = (accountRepository, hashCompare, token) => async ({ email, password }) => {
   const account = await accountRepository.loadByEmail(email)
   if (!account) throw new AuthenticationError()
   const isValid = await hashCompare.comparer({ plaintext: password, digest: account.password })
   if (!isValid) throw new AuthenticationError()
+  await token.generate({ key: account.id })
 }
