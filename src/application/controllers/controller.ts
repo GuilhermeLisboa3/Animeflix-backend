c46ccf866cf39ad42a276bd93d59a5ffaf68223a
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-03-30 13:58:08 -0300

    feat: ensure Controller return 500 if perform throws

--- src/application/controllers/controller.ts
+++ src/application/controllers/controller.ts
@@ -1,12 +1,18 @@
-import { badRequest, HttpResponse } from '@/application/helpers'
+import { badRequest, HttpResponse, serverError } from '@/application/helpers'
 import { ValidationComposite, Validator } from '@/application/validation'
 
 export abstract class Controller {
+  abstract perform (httpRequest: any): Promise<HttpResponse>
   buildValidators (httpRequest: any): Validator[] { return [] }
 
   async handle (httpRequest: any): Promise<HttpResponse | undefined> {
     const error = this.validate(httpRequest)
     if (error !== undefined) return badRequest(error)
+    try {
+      return await this.perform(httpRequest)
+    } catch (error) {
+      return serverError(error)
+    }
   }
 
   private validate (httpRequest: any): Error | undefined {
