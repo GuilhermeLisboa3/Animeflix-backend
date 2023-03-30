60ba6ac800687a609652d23766e8068f1625c40c
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-03-30 13:12:27 -0300

    feat: ensure AddAccountUseCase  return true on success

--- src/domain/usecases/account/add-account.ts
+++ src/domain/usecases/account/add-account.ts
@@ -4,11 +4,12 @@ import { FieldInUseError } from '@/domain/errors'
 
 type Setup = (accountRepository: CheckAccountByEmail & CreateAccount, hash: HashGenerator) => AddAccount
 type Input = { firstName: string, lastName: string, phone: string, birth: Date, email: string, password: string }
-export type AddAccount = (input: Input) => Promise<void>
+type Output = boolean
+export type AddAccount = (input: Input) => Promise<Output>
 
 export const addAccountUseCase: Setup = (accountRepository, hash) => async ({ email, password, birth, firstName, lastName, phone }) => {
   const emailExists = await accountRepository.checkByEmail(email)
   if (emailExists) throw new FieldInUseError('email')
   const hashPassword = await hash.generate({ plaintext: password })
-  await accountRepository.create({ email, password: hashPassword, birth, firstName, lastName, phone })
+  return await accountRepository.create({ email, password: hashPassword, birth, firstName, lastName, phone })
 }
