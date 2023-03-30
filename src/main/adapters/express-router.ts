08a1d42b39dea410f39880eb25de3903ba201081
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-03-30 15:10:25 -0300

    feat: ensure ExpressRoute respond with correct statusCode and data on success

--- src/main/adapters/express-router.ts
+++ src/main/adapters/express-router.ts
@@ -3,5 +3,7 @@ import { Controller } from '@/application/controllers'
 import { RequestHandler } from 'express'
 
 export const expressRouterAdapter = (controller: Controller): RequestHandler => async (req, res) => {
-  await controller.handle({ ...req.body, ...req.params, ...req.query })
+  const { data, statusCode } = await controller.handle({ ...req.body, ...req.params, ...req.query })
+  const json = [200, 204].includes(statusCode) ? data : { error: data.message }
+  res.status(statusCode).json(json)
 }
