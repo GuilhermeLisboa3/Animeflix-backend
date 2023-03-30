8bfd62bef8c422a9655d3691df4839d5f2988026
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-03-30 16:03:51 -0300

    test: ensure LoginController call Authentication with correct input

--- tests/application/controllers/account/login.spec.ts
+++ tests/application/controllers/account/login.spec.ts
@@ -4,13 +4,15 @@ import { EmailValidator, RequiredField } from '@/application/validation'
 describe('LoginController', () => {
   let sut: LoginController
   let makeRequest: { email: string, password: string }
+  let authentication: jest.Mock
 
   beforeAll(() => {
     makeRequest = { email: 'any_email@gmail.com', password: 'any_password' }
+    authentication = jest.fn()
   })
 
   beforeEach(() => {
-    sut = new LoginController()
+    sut = new LoginController(authentication)
   })
 
   it('should build Validators correctly', async () => {
@@ -21,5 +23,12 @@ describe('LoginController', () => {
       new EmailValidator('any_email@gmail.com', 'email'),
       new RequiredField('any_password', 'password')
     ])
+  })
+
+  it('should call Authentication with correct input', async () => {
+    await sut.perform(makeRequest)
+
+    expect(authentication).toHaveBeenCalledWith(makeRequest)
+    expect(authentication).toHaveBeenCalledTimes(1)
   })
 })
