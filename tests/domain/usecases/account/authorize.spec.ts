dfd7d02e6eb67ca5735f448cb06d43e0fc7896d1
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-03-30 16:59:32 -0300

    test: ensure AuthorizeUseCase return AuthenticationError if TokenValidator throw

--- tests/domain/usecases/account/authorize.spec.ts
+++ tests/domain/usecases/account/authorize.spec.ts
@@ -2,8 +2,9 @@ import { Authorize, AuthorizeUseCase } from '@/domain/usecases/account'
 import { TokenValidator } from '@/domain/contracts/gateways'
 
 import { mock, MockProxy } from 'jest-mock-extended'
+import { AuthenticationError } from '@/domain/errors'
 
-describe('AddAccount', () => {
+describe('AuthorizeUseCase', () => {
   let token: MockProxy<TokenValidator>
   let sut: Authorize
   let makeParams: { accessToken: string, role?: string }
@@ -22,5 +23,13 @@ describe('AddAccount', () => {
 
     expect(token.validate).toHaveBeenCalledWith({ token: 'any_token' })
     expect(token.validate).toHaveBeenCalledTimes(1)
+  })
+
+  it('should return AuthenticationError if TokenValidator throw', async () => {
+    token.validate.mockRejectedValueOnce(new Error('any_error'))
+
+    const promise = sut(makeParams)
+
+    await expect(promise).rejects.toThrow(new AuthenticationError())
   })
 })
