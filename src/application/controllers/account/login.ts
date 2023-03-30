0e6601af81e9b78665439197441c470ea28b2c7b
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-03-30 16:03:21 -0300

    feat: ensure LoginController call Authentication with correct input

--- src/application/controllers/account/login.ts
+++ src/application/controllers/account/login.ts
@@ -1,8 +1,15 @@
 import { Validator, ValidationBuilder as build } from '@/application/validation'
+import { Authentication } from '@/domain/usecases/account'
 
 type HttpRequest = { email: string, password: string }
 
 export class LoginController {
+  constructor (private readonly authentication: Authentication) {}
+
+  async perform ({ email, password }: HttpRequest): Promise<void> {
+    await this.authentication({ email, password })
+  }
+
   buildValidators ({ email, password }: HttpRequest): Validator[] {
     return [
       ...build.of(email, 'email').required().email().build(),
