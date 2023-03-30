e994f333e22c47eb6209b65d4ee74dc9e3124d85
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-03-30 16:06:38 -0300

    feat: ensure LoginController extends Controller

--- src/application/controllers/account/login.ts
+++ src/application/controllers/account/login.ts
@@ -1,11 +1,12 @@
+import { Controller } from '@/application/controllers'
 import { HttpResponse, ok } from '@/application/helpers'
 import { Validator, ValidationBuilder as build } from '@/application/validation'
 import { Authentication } from '@/domain/usecases/account'
 
 type HttpRequest = { email: string, password: string }
 
-export class LoginController {
-  constructor (private readonly authentication: Authentication) {}
+export class LoginController extends Controller {
+  constructor (private readonly authentication: Authentication) { super() }
 
   async perform ({ email, password }: HttpRequest): Promise<HttpResponse> {
     const accessToken = await this.authentication({ email, password })
