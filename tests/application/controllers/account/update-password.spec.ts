a45982413dc8fb799df3da9f1ac943a0f2df5d71
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-03-31 13:04:18 -0300

    test: ensure UpdatePasswordController return 204 on success

--- tests/application/controllers/account/update-password.spec.ts
+++ tests/application/controllers/account/update-password.spec.ts
@@ -30,4 +30,10 @@ describe('UpdatePasswordController', () => {
     expect(UpdateAccount).toHaveBeenCalledWith(makeRequest)
     expect(UpdateAccount).toHaveBeenCalledTimes(1)
   })
+
+  it('should return 204 on success', async () => {
+    const httpResponse = await sut.perform(makeRequest)
+
+    expect(httpResponse).toEqual({ statusCode: 204, data: null })
+  })
 })
