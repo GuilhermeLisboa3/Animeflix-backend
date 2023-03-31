791741f9ab53fe262eba2693ca5edc0b463bac4a
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-03-31 18:12:54 -0300

    feat: ensure AwsS3FileStorage return url on success

--- src/infra/gateways/aws-s3-file-storage.ts
+++ src/infra/gateways/aws-s3-file-storage.ts
@@ -2,12 +2,13 @@ import { UploadFile } from '@/domain/contracts/gateways'
 
 import { config, S3 } from 'aws-sdk'
 
-export class AwsS3FileStorage {
+export class AwsS3FileStorage implements UploadFile {
   constructor (accessKeyId: string, secretAccessKey: string, private readonly bucket: string) {
     config.update({ credentials: { accessKeyId, secretAccessKey } })
   }
 
-  async upload ({ file, fileName }: UploadFile.Input): Promise<void> {
+  async upload ({ file, fileName }: UploadFile.Input): Promise<UploadFile.Output> {
     await new S3().upload({ Bucket: this.bucket, Body: file, Key: fileName, ACL: 'public-read' }).promise()
+    return `https://${this.bucket}.s3.amazonaws.com/${encodeURIComponent(fileName)}`
   }
 }
