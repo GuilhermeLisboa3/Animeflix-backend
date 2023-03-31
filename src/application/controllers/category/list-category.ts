680362c711e055cfa5f65fe31e5bf4c227a09105
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-03-31 15:10:29 -0300

    feat: ensure ListCategoryController extends Controller

--- src/application/controllers/category/list-category.ts
+++ src/application/controllers/category/list-category.ts
@@ -1,13 +1,14 @@
 import { HttpResponse, ok } from '@/application/helpers'
 import { ListCategory } from '@/domain/usecases/category'
+import { Controller } from '@/application/controllers'
 
 type HttpRequest = {
   page?: string
   perPage?: string
 }
 
-export class ListCategoryController {
-  constructor (private readonly listCategory: ListCategory) {}
+export class ListCategoryController extends Controller {
+  constructor (private readonly listCategory: ListCategory) { super() }
 
   async perform ({ page, perPage }: HttpRequest): Promise<HttpResponse> {
     const listCategories = await this.listCategory({ page, perPage })
