8709b6f56b408e6e672727dfd715725fd30081d8
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-03-31 13:23:13 -0300

    feat: ensure GetAccountDataController return 200 on success

--- src/application/controllers/account/get-account-data.ts
+++ src/application/controllers/account/get-account-data.ts
@@ -1,3 +1,4 @@
+import { HttpResponse, ok } from '@/application/helpers'
 import { GetAccountData } from '@/domain/usecases/account'
 
 type HttpRequest = { accountId: string }
@@ -5,7 +6,8 @@ type HttpRequest = { accountId: string }
 export class GetAccountDataController {
   constructor (private readonly getAccountData: GetAccountData) { }
 
-  async perform ({ accountId }: HttpRequest): Promise<void> {
-    await this.getAccountData({ id: accountId })
+  async perform ({ accountId }: HttpRequest): Promise<HttpResponse> {
+    const account = await this.getAccountData({ id: accountId })
+    return ok(account)
   }
 }
