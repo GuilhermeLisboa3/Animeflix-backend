59d51bae5dd1ca675ffbd0a1001dedd78826f3db
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-03-31 13:53:21 -0300

    feat: ensure AddCategoryUseCase return FieldInUseError if CheckCategory return true

--- src/domain/usecases/category/add-category.ts
+++ src/domain/usecases/category/add-category.ts
@@ -1,4 +1,5 @@
 import { CheckCategory } from '@/domain/contracts/database/category'
+import { FieldInUseError } from '@/domain/errors'
 
 type Setup = (categoryRepository: CheckCategory) => AddCategory
 type Input = { name: string, position: number }
@@ -6,5 +7,6 @@ export type AddCategory = (input: Input) => Promise<void>
 
 export const AddCategoryUseCase: Setup = (categoryRepository) => async ({ name, position }) => {
   const nameLowerCase = name.toLocaleLowerCase()
-  await categoryRepository.check({ name: nameLowerCase, position })
+  const categoryExists = await categoryRepository.check({ name: nameLowerCase, position })
+  if (categoryExists) throw new FieldInUseError('name or position')
 }
