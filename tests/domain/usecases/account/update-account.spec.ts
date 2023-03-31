5ad4218bf2fcc01d5b8199c015473fbacb61d824
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-03-31 11:57:49 -0300

    test: ensure UpdateAccountUseCase call HashComparer with correct input

--- tests/domain/usecases/account/update-account.spec.ts
+++ tests/domain/usecases/account/update-account.spec.ts
@@ -1,4 +1,5 @@
 import { LoadAccountById } from '@/domain/contracts/database/account'
+import { HashComparer } from '@/domain/contracts/gateways'
 import { UpdateAccountUseCase, UpdateAccount } from '@/domain/usecases/account'
 
 import { mock, MockProxy } from 'jest-mock-extended'
@@ -6,15 +7,18 @@ import { mock, MockProxy } from 'jest-mock-extended'
 describe('Update Account', () => {
   let accountRepository: MockProxy<LoadAccountById>
   let sut: UpdateAccount
+  let hash: MockProxy<HashComparer>
   let makeAccount: { accountId: string, currentPassword: string, newPassword: string }
 
   beforeAll(() => {
     makeAccount = { accountId: 'any_id', currentPassword: 'any_password', newPassword: 'new_password' }
     accountRepository = mock()
+    accountRepository.loadById.mockResolvedValue({ firstName: 'any_name', lastName: 'any_last_name', email: 'any_email', password: 'account_password', birth: new Date(), phone: 'any_phone', role: 'user' })
+    hash = mock()
   })
 
   beforeEach(() => {
-    sut = UpdateAccountUseCase(accountRepository)
+    sut = UpdateAccountUseCase(accountRepository, hash)
   })
 
   it('should call loadAccountById with correct input', async () => {
@@ -22,5 +26,12 @@ describe('Update Account', () => {
 
     expect(accountRepository.loadById).toHaveBeenCalledWith({ id: 'any_id' })
     expect(accountRepository.loadById).toHaveBeenCalledTimes(1)
+  })
+
+  it('should call HashComparer with correct input', async () => {
+    await sut(makeAccount)
+
+    expect(hash.comparer).toHaveBeenCalledWith({ plaintext: 'any_password', digest: 'account_password' })
+    expect(hash.comparer).toHaveBeenCalledTimes(1)
   })
 })
