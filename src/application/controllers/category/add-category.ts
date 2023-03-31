d672e536bfe1ab6dbe1f3e30e4350d73af969df6
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-03-31 14:23:31 -0300

    feat: ensure AddCategoryController return 204 on success

--- src/application/controllers/category/add-category.ts
+++ src/application/controllers/category/add-category.ts
@@ -1,3 +1,4 @@
+import { HttpResponse, noContent } from '@/application/helpers'
 import { Validator, ValidationBuilder as builder } from '@/application/validation'
 import { AddCategory } from '@/domain/usecases/category'
 
@@ -9,8 +10,9 @@ type HttpRequest = {
 export class AddCategoryController {
   constructor (private readonly addCategory: AddCategory) {}
 
-  async perform ({ name, position }: HttpRequest): Promise<void> {
+  async perform ({ name, position }: HttpRequest): Promise<HttpResponse> {
     await this.addCategory({ name, position })
+    return noContent()
   }
 
   buildValidators ({ name, position }: HttpRequest): Validator[] {
