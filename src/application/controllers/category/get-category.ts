f09cf7477e1f19c849f4331a375c1ee3175e6b5b
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-03-31 16:31:23 -0300

    feat: ensure GetCategoryController extends Controller

--- src/application/controllers/category/get-category.ts
+++ src/application/controllers/category/get-category.ts
@@ -1,10 +1,11 @@
+import { Controller } from '@/application/controllers'
 import { HttpResponse, ok } from '@/application/helpers'
 import { LoadCategory } from '@/domain/usecases/category'
 
 type HttpRequest = { id: string }
 
-export class GetCategoryController {
-  constructor (private readonly loadCategory: LoadCategory) {}
+export class GetCategoryController extends Controller {
+  constructor (private readonly loadCategory: LoadCategory) { super() }
 
   async perform ({ id }: HttpRequest): Promise<HttpResponse> {
     const category = await this.loadCategory({ id })
