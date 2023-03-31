c3bdf4b1dd278753e85c0b548fe5986814cf88af
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-03-31 16:03:40 -0300

    feat: ensure DeleteCategoryController call DeleteCategory with correct input

--- src/application/controllers/category/delete-category.ts
+++ src/application/controllers/category/delete-category.ts
@@ -1,10 +1,17 @@
 import { Validator, ValidationBuilder as builder } from '@/application/validation'
+import { DeleteCategory } from '@/domain/usecases/category'
 
 type HttpRequest = {
   id: string
 }
 
 export class DeleteCategoryController {
+  constructor (private readonly deleteCategory: DeleteCategory) {}
+
+  async perform ({ id }: HttpRequest): Promise<void> {
+    await this.deleteCategory({ id })
+  }
+
   buildValidators ({ id }: HttpRequest): Validator[] {
     return [
       ...builder.of(id, 'id').required().build()
