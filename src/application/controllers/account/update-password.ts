8ae2b665a0288ad8326fc6c20f97af90d98f260c
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-03-31 13:02:01 -0300

    feat: ensure UpdatePasswordController call UpdateAccount with correct input

--- src/application/controllers/account/update-password.ts
+++ src/application/controllers/account/update-password.ts
@@ -1,4 +1,5 @@
 import { Validator, ValidationBuilder as builder } from '@/application/validation'
+import { UpdateAccount } from '@/domain/usecases/account'
 
 type HttpRequest = {
   accountId: string
@@ -7,6 +8,12 @@ type HttpRequest = {
 }
 
 export class UpdatePasswordController {
+  constructor (private readonly updateAccount: UpdateAccount) {}
+
+  async perform (input: HttpRequest): Promise<void> {
+    await this.updateAccount(input)
+  }
+
   buildValidators ({ currentPassword, newPassword }: HttpRequest): Validator[] {
     return [
       ...builder.of(currentPassword, 'currentPassword').required().build(),
