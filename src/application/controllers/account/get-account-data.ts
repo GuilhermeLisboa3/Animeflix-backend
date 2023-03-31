34ffadce36896db926754e70a5e1aef34c8bfe87
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-03-31 13:25:05 -0300

    feat: ensure GetAccountDataController extends Controller

--- src/application/controllers/account/get-account-data.ts
+++ src/application/controllers/account/get-account-data.ts
@@ -1,10 +1,11 @@
+import { Controller } from '@/application/controllers'
 import { HttpResponse, ok } from '@/application/helpers'
 import { GetAccountData } from '@/domain/usecases/account'
 
 type HttpRequest = { accountId: string }
 
-export class GetAccountDataController {
-  constructor (private readonly getAccountData: GetAccountData) { }
+export class GetAccountDataController extends Controller {
+  constructor (private readonly getAccountData: GetAccountData) { super() }
 
   async perform ({ accountId }: HttpRequest): Promise<HttpResponse> {
     const account = await this.getAccountData({ id: accountId })
