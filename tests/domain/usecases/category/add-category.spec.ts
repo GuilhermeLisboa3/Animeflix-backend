3d59e8d1fe46bc8eda263ded290b722eafb9c31a
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-03-31 13:56:54 -0300

    test: ensure AddCategoryUseCase call CreateCategory with correct input

--- tests/domain/usecases/category/add-category.spec.ts
+++ tests/domain/usecases/category/add-category.spec.ts
@@ -1,11 +1,11 @@
 import { AddCategoryUseCase, AddCategory } from '@/domain/usecases/category'
-import { CheckCategory } from '@/domain/contracts/database/category'
+import { CheckCategory, CreateCategory } from '@/domain/contracts/database/category'
 
 import { mock, MockProxy } from 'jest-mock-extended'
 import { FieldInUseError } from '@/domain/errors'
 
 describe('AddCategory', () => {
-  let categoryRepository: MockProxy<CheckCategory>
+  let categoryRepository: MockProxy<CheckCategory & CreateCategory>
   let category: { name: string, position: number }
   let sut: AddCategory
 
@@ -32,5 +32,12 @@ describe('AddCategory', () => {
     const promise = sut(category)
 
     await expect(promise).rejects.toThrow(new FieldInUseError('name or position'))
+  })
+
+  it('should call CreateCategory with correct input', async () => {
+    await sut(category)
+
+    expect(categoryRepository.create).toHaveBeenCalledWith({ name: 'any_name', position: 1 })
+    expect(categoryRepository.create).toHaveBeenCalledTimes(1)
   })
 })
