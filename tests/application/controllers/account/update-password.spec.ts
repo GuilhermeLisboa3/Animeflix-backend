c5c0054952112e49db90ba97b0f89a16504b1da7
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-03-31 13:08:30 -0300

    test: ensure UpdatePasswordController return 400 if UpdateAccount returns CompareFieldsError

--- tests/application/controllers/account/update-password.spec.ts
+++ tests/application/controllers/account/update-password.spec.ts
@@ -1,6 +1,7 @@
 import { UpdatePasswordController } from '@/application/controllers/account'
 import { Controller } from '@/application/controllers'
 import { RequiredField } from '@/application/validation'
+import { CompareFieldsError } from '@/domain/errors'
 
 describe('UpdatePasswordController', () => {
   let sut: UpdatePasswordController
@@ -34,6 +35,17 @@ describe('UpdatePasswordController', () => {
 
     expect(UpdateAccount).toHaveBeenCalledWith(makeRequest)
     expect(UpdateAccount).toHaveBeenCalledTimes(1)
+  })
+
+  it('should return 400 if UpdateAccount returns CompareFieldsError', async () => {
+    UpdateAccount.mockRejectedValueOnce(new CompareFieldsError('currentPassword', 'password'))
+
+    const httpResponse = await sut.handle(makeRequest)
+
+    expect(httpResponse).toEqual({
+      statusCode: 400,
+      data: new CompareFieldsError('currentPassword', 'password')
+    })
   })
 
   it('should return 204 on success', async () => {
