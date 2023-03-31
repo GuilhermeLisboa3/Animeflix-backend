d3b7d7142a650ebaf80ef4989169baee4ba7a029
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-03-31 17:40:51 -0300

    feat: ensure AddAnimeUseCase call UploadFile with correct input

--- src/domain/usecases/anime/add-anime.ts
+++ src/domain/usecases/anime/add-anime.ts
@@ -1,17 +1,23 @@
 import { CheckAnime } from '@/domain/contracts/database/anime'
 import { CheckCategoryById } from '@/domain/contracts/database/category'
-import { UUIDGenerator } from '@/domain/contracts/gateways'
+import { UploadFile, UUIDGenerator } from '@/domain/contracts/gateways'
 import { FieldInUseError, NotFoundError } from '@/domain/errors'
 
-type Setup = (animeRepository: CheckAnime, categoryRepository: CheckCategoryById, uuid: UUIDGenerator) => AddAnime
+type Setup = (
+  animeRepository: CheckAnime,
+  categoryRepository: CheckCategoryById,
+  uuid: UUIDGenerator,
+  fileStorage: UploadFile
+) => AddAnime
 type Input = { name: string, categoryId: number, file?: { buffer: Buffer, mimeType: string }, synopsis: string, featured?: boolean }
 export type AddAnime = (input: Input) => Promise<void>
 
-export const AddAnimeUseCase: Setup = (animeRepository, categoryRepository, uuid) => async ({ name, categoryId, file, synopsis, featured }) => {
+export const AddAnimeUseCase: Setup = (animeRepository, categoryRepository, uuid, fileStorage) => async ({ name, categoryId, file, synopsis, featured }) => {
   const nameLowerCase = name.toLocaleLowerCase()
   const animeExists = await animeRepository.check({ name: nameLowerCase })
   if (animeExists) throw new FieldInUseError('name')
   const categoryExists = await categoryRepository.checkById({ id: categoryId })
   if (!categoryExists) throw new NotFoundError('categoryId')
-  uuid.generate()
+  const key = uuid.generate()
+  if (file) await fileStorage.upload({ file: file.buffer, fileName: `${key}.${file.mimeType.split('/')[1]}` })
 }
