8fc69a26ec38fb69aad35b55725529b5fc2ea7b8
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-03-31 11:56:52 -0300

    feat: ensure UpdateAccountUseCase call HashComparer with correct input

--- src/domain/usecases/account/update-account.ts
+++ src/domain/usecases/account/update-account.ts
@@ -1,11 +1,15 @@
 import { LoadAccountById } from '@/domain/contracts/database/account'
+import { HashComparer } from '@/domain/contracts/gateways'
 
-type Setup = (accountRepository: LoadAccountById) => UpdateAccount
+type Setup = (accountRepository: LoadAccountById, hash: HashComparer) => UpdateAccount
 type Input = { accountId: string, currentPassword?: string, newPassword?: string, firstName?: string, lastName?: string, phone?: string, birth?: Date, email?: string }
 export type UpdateAccount = (input: Input) => Promise<void>
 
-export const UpdateAccountUseCase: Setup = (accountRepository) => async ({ accountId, currentPassword, newPassword, firstName, lastName, email, birth, phone }) => {
+export const UpdateAccountUseCase: Setup = (accountRepository, hash) => async ({ accountId, currentPassword, newPassword, firstName, lastName, email, birth, phone }) => {
   if (currentPassword && newPassword) {
-    await accountRepository.loadById({ id: accountId })
+    const account = await accountRepository.loadById({ id: accountId })
+    if (account) {
+      await hash.comparer({ plaintext: currentPassword, digest: account.password })
+    }
   }
 }
