435e6eadd56215fe5e1170ccad74419875467fed
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-03-31 15:09:08 -0300

    feat: ensure ListCategoryController return 200 on success

--- src/application/controllers/category/list-category.ts
+++ src/application/controllers/category/list-category.ts
@@ -1,3 +1,4 @@
+import { HttpResponse, ok } from '@/application/helpers'
 import { ListCategory } from '@/domain/usecases/category'
 
 type HttpRequest = {
@@ -8,7 +9,8 @@ type HttpRequest = {
 export class ListCategoryController {
   constructor (private readonly listCategory: ListCategory) {}
 
-  async perform ({ page, perPage }: HttpRequest): Promise<void> {
-    await this.listCategory({ page, perPage })
+  async perform ({ page, perPage }: HttpRequest): Promise<HttpResponse> {
+    const listCategories = await this.listCategory({ page, perPage })
+    return ok(listCategories)
   }
 }
