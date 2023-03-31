106c1fbb82d320738eb5174ac9585d356fc5defe
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-03-31 14:12:53 -0300

    feat: ensure CategoryRepository create category on success

--- src/infra/database/postgres/repositories/category.ts
+++ src/infra/database/postgres/repositories/category.ts
@@ -1,9 +1,14 @@
-import { CheckCategory } from '@/domain/contracts/database/category'
+import { CheckCategory, CreateCategory } from '@/domain/contracts/database/category'
 import { Category } from '@/infra/database/postgres/entities'
 
-export class CategoryRepository implements CheckCategory {
+export class CategoryRepository implements CheckCategory, CreateCategory {
   async check ({ name, position }: CheckCategory.Input): Promise<CheckCategory.Output> {
     const existAccount = await Category.findOne({ where: { name, position } })
     return existAccount !== null
+  }
+
+  async create ({ name, position }: CreateCategory.Input): Promise<CreateCategory.Output> {
+    const createCategory = await Category.create({ name, position })
+    return createCategory !== null
   }
 }
