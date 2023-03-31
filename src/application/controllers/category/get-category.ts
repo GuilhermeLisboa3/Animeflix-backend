382dc31445e84d96a8702243c4f19897940ce60f
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-03-31 16:28:50 -0300

    feat: ensure GetCategoryController return 200 on success

--- src/application/controllers/category/get-category.ts
+++ src/application/controllers/category/get-category.ts
@@ -1,3 +1,4 @@
+import { HttpResponse, ok } from '@/application/helpers'
 import { LoadCategory } from '@/domain/usecases/category'
 
 type HttpRequest = { id: string }
@@ -5,7 +6,8 @@ type HttpRequest = { id: string }
 export class GetCategoryController {
   constructor (private readonly loadCategory: LoadCategory) {}
 
-  async perform ({ id }: HttpRequest): Promise<void> {
-    await this.loadCategory({ id })
+  async perform ({ id }: HttpRequest): Promise<HttpResponse> {
+    const category = await this.loadCategory({ id })
+    return ok(category)
   }
 }
