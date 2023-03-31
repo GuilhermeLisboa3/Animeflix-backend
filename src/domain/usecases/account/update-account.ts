dd5c1a69a640842af2335fa7c79566b9e044b54d
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-03-31 12:04:57 -0300

    feat: ensure UpdateAccountUseCase call UpdateAccount with correct input

--- src/domain/usecases/account/update-account.ts
+++ src/domain/usecases/account/update-account.ts
@@ -1,18 +1,20 @@
-import { LoadAccountById } from '@/domain/contracts/database/account'
+import { LoadAccountById, UpdateAccountById } from '@/domain/contracts/database/account'
 import { HashComparer, HashGenerator } from '@/domain/contracts/gateways'
 import { CompareFieldsError } from '@/domain/errors'
 
-type Setup = (accountRepository: LoadAccountById, hash: HashComparer & HashGenerator) => UpdateAccount
+type Setup = (accountRepository: LoadAccountById & UpdateAccountById, hash: HashComparer & HashGenerator) => UpdateAccount
 type Input = { accountId: string, currentPassword?: string, newPassword?: string, firstName?: string, lastName?: string, phone?: string, birth?: Date, email?: string }
 export type UpdateAccount = (input: Input) => Promise<void>
 
 export const UpdateAccountUseCase: Setup = (accountRepository, hash) => async ({ accountId, currentPassword, newPassword, firstName, lastName, email, birth, phone }) => {
+  let password: string | undefined
   if (currentPassword && newPassword) {
     const account = await accountRepository.loadById({ id: accountId })
     if (account) {
       const isValid = await hash.comparer({ plaintext: currentPassword, digest: account.password })
       if (!isValid) throw new CompareFieldsError('currentPassword', 'password')
-      await hash.generate({ plaintext: newPassword })
+      password = await hash.generate({ plaintext: newPassword })
     }
   }
+  await accountRepository.update({ id: accountId, password, firstName, lastName, email, birth, phone })
 }
