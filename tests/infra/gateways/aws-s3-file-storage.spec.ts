535eafc8527ed27750e6ae28cecabd1f781b0fcb
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-03-31 18:10:25 -0300

    test: ensure AwsS3FileStorage call putObject with correct values

--- tests/infra/gateways/aws-s3-file-storage.spec.ts
+++ tests/infra/gateways/aws-s3-file-storage.spec.ts
@@ -1,6 +1,6 @@
 import { AwsS3FileStorage } from '@/infra/gateways'
 
-import { config } from 'aws-sdk'
+import { config, S3 } from 'aws-sdk'
 
 jest.mock('aws-sdk')
 
@@ -8,19 +8,44 @@ describe('AwsS3FileStorage', () => {
   let sut: AwsS3FileStorage
   let accessKeyId: string
   let secretAccessKey: string
+  let bucket: string
 
   beforeAll(() => {
     accessKeyId = 'any_access_key'
     secretAccessKey = 'any_secret'
+    bucket = 'any_bucket'
   })
 
   beforeEach(() => {
-    sut = new AwsS3FileStorage(accessKeyId, secretAccessKey)
+    sut = new AwsS3FileStorage(accessKeyId, secretAccessKey, bucket)
   })
 
   it('should config aws credentials on creation', () => {
     expect(sut)
     expect(config.update).toHaveBeenCalledWith({ credentials: { accessKeyId, secretAccessKey } })
     expect(config.update).toHaveBeenCalledTimes(1)
+  })
+
+  describe('upload', () => {
+    let file: Buffer
+    let fileName: string
+    let promiseSpy: jest.Mock
+    let uploadSpy: jest.Mock
+
+    beforeAll(() => {
+      file = Buffer.from('any_value')
+      fileName = 'any_name'
+      promiseSpy = jest.fn()
+      uploadSpy = jest.fn().mockImplementation(jest.fn().mockImplementation(() => ({ promise: promiseSpy })))
+      jest.mocked(S3).mockImplementation(jest.fn().mockImplementation(() => ({ upload: uploadSpy })))
+    })
+
+    it('should call putObject with correct values', async () => {
+      await sut.upload({ file, fileName })
+
+      expect(uploadSpy).toHaveBeenCalledWith({ Bucket: bucket, Body: file, Key: fileName, ACL: 'public-read' })
+      expect(uploadSpy).toHaveBeenCalledTimes(1)
+      expect(promiseSpy).toHaveBeenCalledTimes(1)
+    })
   })
 })
