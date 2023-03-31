d56062c83a89ce7a611ef15b45dfad90120001ee
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-03-31 16:32:38 -0300

    feat: ensure GetCategoryController return 400 if LoadCategory returns NotFoundError

--- src/application/controllers/category/get-category.ts
+++ src/application/controllers/category/get-category.ts
@@ -1,5 +1,6 @@
 import { Controller } from '@/application/controllers'
 import { HttpResponse, ok } from '@/application/helpers'
+import { NotFoundError } from '@/domain/errors'
 import { LoadCategory } from '@/domain/usecases/category'
 
 type HttpRequest = { id: string }
@@ -9,6 +10,7 @@ export class GetCategoryController extends Controller {
 
   async perform ({ id }: HttpRequest): Promise<HttpResponse> {
     const category = await this.loadCategory({ id })
+    if (!category) throw new NotFoundError('category')
     return ok(category)
   }
 }
