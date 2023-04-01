5deff4f196343776aa33a3ef0a53233ca9da5f95
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-04-01 17:00:30 -0300

    test: ensure route post /anime return 204 on success

---
+++ tests/main/routes/anime.test.ts
@@ -0,0 +1,43 @@
+import { app } from '@/main/config/app'
+import env from '@/main/config/env'
+import { sequelize, Account, Category } from '@/infra/database/postgres/entities'
+
+import request from 'supertest'
+import { sign } from 'jsonwebtoken'
+import MockDate from 'mockdate'
+
+describe('AnimeRoute', () => {
+  let token: string
+  const makeAccount = { firstName: 'any_name', lastName: 'any_last_name', email: 'any_email@gmail.com', password: 'any_password', birth: new Date(), phone: 'any_phone' }
+  const makeCategory = { name: 'any_category', position: 1 }
+
+  beforeAll(async () => {
+    MockDate.set(new Date())
+    token = sign({ key: '1' }, env.secret)
+  })
+
+  beforeEach(async () => {
+    await sequelize.sync({ force: true })
+    await Account.create({ ...makeAccount, role: 'admin' })
+    await Category.create(makeCategory)
+  })
+
+  afterAll(async () => {
+    MockDate.reset()
+    await sequelize.close()
+  })
+
+  describe('POST /anime', () => {
+    it('should return 204 on success', async () => {
+      const { status, body } = await request(app)
+        .post('/anime')
+        .set({ authorization: `Bearer: ${token}` })
+        .field('name', 'any_anime')
+        .field('categoryId', 1)
+        .field('synopsis', 'any_synopsis')
+
+      expect(status).toBe(204)
+      expect(body).toBeTruthy()
+    })
+  })
+})
