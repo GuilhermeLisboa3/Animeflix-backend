35ad5bae010aa83642c3649033c7a6a8542fa01f
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-04-01 17:51:29 -0300

    feat: ensure AnimeRepository return anime on success

--- src/infra/database/postgres/repositories/anime.ts
+++ src/infra/database/postgres/repositories/anime.ts
@@ -1,7 +1,7 @@
-import { CheckAnime, CreateAnime } from '@/domain/contracts/database/anime'
+import { CheckAnime, CreateAnime, LoadAnimeById } from '@/domain/contracts/database/anime'
 import { Anime } from '@/infra/database/postgres/entities'
 
-export class AnimeRepository implements CheckAnime, CreateAnime {
+export class AnimeRepository implements CheckAnime, CreateAnime, LoadAnimeById {
   async check ({ name }: CheckAnime.Input): Promise<CheckAnime.Output> {
     const existAnime = await Anime.findOne({ where: { name } })
     return existAnime !== null
@@ -10,5 +10,10 @@ export class AnimeRepository implements CheckAnime, CreateAnime {
   async create ({ name, categoryId, synopsis, featured, thumbnailUrl }: CreateAnime.Input): Promise<CreateAnime.Output> {
     const anime = await Anime.create({ name, categoryId, synopsis, featured, thumbnailUrl })
     return anime !== null
+  }
+
+  async loadById ({ id }: LoadAnimeById.Input): Promise<LoadAnimeById.Output> {
+    const anime = await Anime.findByPk(id, { attributes: ['id', 'name', 'synopsis', ['thumbnail_url', 'thumbnailUrl']] })
+    return anime !== null ? anime : undefined
   }
 }
