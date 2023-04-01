7d896dc38ea02cb5a64965bbfaa9acf2697d7a30
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-04-01 17:43:34 -0300

    feat: ensure DeleteAnimeUseCase call DeleteFile with correct input

--- src/domain/usecases/anime/delete-anime.ts
+++ src/domain/usecases/anime/delete-anime.ts
@@ -1,11 +1,15 @@
 import { LoadAnimeById } from '@/domain/contracts/database/anime'
+import { DeleteFile } from '@/domain/contracts/gateways'
 import { NotFoundError } from '@/domain/errors'
 
-type Setup = (animeRepository: LoadAnimeById) => DeleteAnime
+type Setup = (animeRepository: LoadAnimeById, fileStorage: DeleteFile) => DeleteAnime
 type Input = { id: string }
 export type DeleteAnime = (input: Input) => Promise<void>
 
-export const DeleteAnimeUseCase: Setup = (animeRepository) => async ({ id }) => {
+export const DeleteAnimeUseCase: Setup = (animeRepository, fileStorage) => async ({ id }) => {
   const anime = await animeRepository.loadById({ id })
   if (!anime) throw new NotFoundError('id')
+  if (anime.thumbnailUrl !== null) {
+    await fileStorage.delete({ fileName: anime.thumbnailUrl })
+  }
 }
