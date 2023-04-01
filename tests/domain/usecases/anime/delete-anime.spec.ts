d99dd099aeea15780c81b3dab81e66f18c87472a
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-04-01 17:44:43 -0300

    test: ensure DeleteAnimeUseCase call DeleteFile with correct input

--- tests/domain/usecases/anime/delete-anime.spec.ts
+++ tests/domain/usecases/anime/delete-anime.spec.ts
@@ -1,11 +1,13 @@
 import { DeleteAnimeUseCase, DeleteAnime } from '@/domain/usecases/anime'
 import { LoadAnimeById } from '@/domain/contracts/database/anime'
+import { DeleteFile } from '@/domain/contracts/gateways'
 
 import { MockProxy, mock } from 'jest-mock-extended'
 import { NotFoundError } from '@/domain/errors'
 
 describe('DeleteAnimeUseCase', () => {
   let animeRepository: MockProxy<LoadAnimeById>
+  let fileStorage: MockProxy<DeleteFile>
   let makeAnime: { id: string }
   let sut: DeleteAnime
 
@@ -13,10 +15,11 @@ describe('DeleteAnimeUseCase', () => {
     makeAnime = { id: '1' }
     animeRepository = mock()
     animeRepository.loadById.mockResolvedValue({ id: 1, name: 'any_name', synopsis: 'any_synopsis', thumbnailUrl: 'any_value' })
+    fileStorage = mock()
   })
 
   beforeEach(() => {
-    sut = DeleteAnimeUseCase(animeRepository)
+    sut = DeleteAnimeUseCase(animeRepository, fileStorage)
   })
 
   it('should call LoadAnimeById with correct input', async () => {
@@ -32,5 +35,12 @@ describe('DeleteAnimeUseCase', () => {
     const promise = sut(makeAnime)
 
     await expect(promise).rejects.toThrow(new NotFoundError('id'))
+  })
+
+  it('should call DeleteFile with correct input', async () => {
+    await sut(makeAnime)
+
+    expect(fileStorage.delete).toHaveBeenCalledWith({ fileName: 'any_value' })
+    expect(fileStorage.delete).toHaveBeenCalledTimes(1)
   })
 })
