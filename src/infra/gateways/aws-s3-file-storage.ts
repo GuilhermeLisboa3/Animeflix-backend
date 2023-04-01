5ff9e5e9ef9e6de51522d0644e6a84f3f6a8818b
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-04-01 12:01:01 -0300

    refactor: ensure AwsS3FileStorage implements DeleteFile

--- src/infra/gateways/aws-s3-file-storage.ts
+++ src/infra/gateways/aws-s3-file-storage.ts
@@ -2,7 +2,7 @@ import { DeleteFile, UploadFile } from '@/domain/contracts/gateways'
 
 import { config, S3 } from 'aws-sdk'
 
-export class AwsS3FileStorage implements UploadFile {
+export class AwsS3FileStorage implements UploadFile, DeleteFile {
   constructor (accessKeyId: string, secretAccessKey: string, private readonly bucket: string) {
     config.update({ credentials: { accessKeyId, secretAccessKey } })
   }
