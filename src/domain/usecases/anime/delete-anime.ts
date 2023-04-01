802f85a6756fdf6198cba753f81062630da8448f
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-04-01 17:41:29 -0300

    feat: ensure DeleteAnimeUseCase return NotFoundError if LoadAnimeById returns undefined

--- src/domain/usecases/anime/delete-anime.ts
+++ src/domain/usecases/anime/delete-anime.ts
@@ -1,9 +1,11 @@
 import { LoadAnimeById } from '@/domain/contracts/database/anime'
+import { NotFoundError } from '@/domain/errors'
 
 type Setup = (animeRepository: LoadAnimeById) => DeleteAnime
 type Input = { id: string }
 export type DeleteAnime = (input: Input) => Promise<void>
 
 export const DeleteAnimeUseCase: Setup = (animeRepository) => async ({ id }) => {
-  await animeRepository.loadById({ id })
+  const anime = await animeRepository.loadById({ id })
+  if (!anime) throw new NotFoundError('id')
 }
