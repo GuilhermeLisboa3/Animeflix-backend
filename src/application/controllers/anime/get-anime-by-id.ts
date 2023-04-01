1c2565d4de54295ce415dab9cbbdee7e9872e9d8
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-04-01 19:37:02 -0300

    feat: ensure GetAnimeByIdController return 200 on success

--- src/application/controllers/anime/get-anime-by-id.ts
+++ src/application/controllers/anime/get-anime-by-id.ts
@@ -1,3 +1,4 @@
+import { HttpResponse, ok } from '@/application/helpers'
 import { Validator, ValidationBuilder as builder } from '@/application/validation'
 import { GetAnimeById } from '@/domain/usecases/anime'
 
@@ -6,8 +7,9 @@ type HttpRequest = { id: string }
 export class GetAnimeByIdController {
   constructor (private readonly getAnimeById: GetAnimeById) {}
 
-  async perform ({ id }: HttpRequest): Promise<void> {
-    await this.getAnimeById({ id })
+  async perform ({ id }: HttpRequest): Promise<HttpResponse> {
+    const anime = await this.getAnimeById({ id })
+    return ok(anime)
   }
 
   buildValidators ({ id }: HttpRequest): Validator[] {
