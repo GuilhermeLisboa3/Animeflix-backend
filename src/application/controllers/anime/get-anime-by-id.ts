e62f19ccd8a8004d3c176138650e335388abf624
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-04-01 19:33:21 -0300

    feat: ensure GetAnimeByIdController call GetAnimeById with correct input

--- src/application/controllers/anime/get-anime-by-id.ts
+++ src/application/controllers/anime/get-anime-by-id.ts
@@ -1,8 +1,15 @@
 import { Validator, ValidationBuilder as builder } from '@/application/validation'
+import { GetAnimeById } from '@/domain/usecases/anime'
 
 type HttpRequest = { id: string }
 
 export class GetAnimeByIdController {
+  constructor (private readonly getAnimeById: GetAnimeById) {}
+
+  async perform ({ id }: HttpRequest): Promise<void> {
+    await this.getAnimeById({ id })
+  }
+
   buildValidators ({ id }: HttpRequest): Validator[] {
     return [
       ...builder.of(id, 'id').required().build()
