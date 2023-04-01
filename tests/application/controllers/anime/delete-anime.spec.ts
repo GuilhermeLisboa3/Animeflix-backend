01f39c1cb2c7eec48d73c515a679f50af9ebab69
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-04-01 18:01:32 -0300

    test: ensure DeleteAnimeController call DeleteAnime with correct input

--- tests/application/controllers/anime/delete-anime.spec.ts
+++ tests/application/controllers/anime/delete-anime.spec.ts
@@ -4,13 +4,15 @@ import { RequiredField } from '@/application/validation'
 describe('DeleteAnimeController', () => {
   let sut: DeleteAnimeController
   let makeRequest: { id: string }
+  let DeleteAnime: jest.Mock
 
   beforeAll(() => {
     makeRequest = { id: '1' }
+    DeleteAnime = jest.fn()
   })
 
   beforeEach(() => {
-    sut = new DeleteAnimeController()
+    sut = new DeleteAnimeController(DeleteAnime)
   })
 
   it('should build Validators correctly', async () => {
@@ -19,5 +21,12 @@ describe('DeleteAnimeController', () => {
     expect(validators).toEqual([
       new RequiredField('1', 'id')
     ])
+  })
+
+  it('should call DeleteAnime with correct input', async () => {
+    await sut.perform(makeRequest)
+
+    expect(DeleteAnime).toHaveBeenCalledWith({ id: '1' })
+    expect(DeleteAnime).toHaveBeenCalledTimes(1)
   })
 })
