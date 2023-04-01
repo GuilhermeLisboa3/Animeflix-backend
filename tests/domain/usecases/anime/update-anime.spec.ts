d7ca7f653cc27baf80a020c2b83caba731dae379
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-04-01 18:28:06 -0300

    test: ensure UpdateAnimeUseCase return NotFoundError if LoadAnimeById returns undefined

--- tests/domain/usecases/anime/update-anime.spec.ts
+++ tests/domain/usecases/anime/update-anime.spec.ts
@@ -2,6 +2,7 @@ import { UpdateAnimeUseCase, UpdateAnime } from '@/domain/usecases/anime'
 import { LoadAnimeById } from '@/domain/contracts/database/anime'
 
 import { MockProxy, mock } from 'jest-mock-extended'
+import { NotFoundError } from '@/domain/errors'
 
 describe('UpdateAnimeUseCase', () => {
   let animeRepository: MockProxy<LoadAnimeById>
@@ -11,6 +12,7 @@ describe('UpdateAnimeUseCase', () => {
   beforeAll(() => {
     makeAnime = { id: '1', file: { buffer: Buffer.from('any'), mimeType: 'image/png' }, categoryId: 1 }
     animeRepository = mock()
+    animeRepository.loadById.mockResolvedValue({ id: 1, name: 'any_name', synopsis: 'any_synopsis', thumbnailUrl: 'any_value' })
   })
 
   beforeEach(() => {
@@ -22,5 +24,13 @@ describe('UpdateAnimeUseCase', () => {
 
     expect(animeRepository.loadById).toHaveBeenCalledWith({ id: '1' })
     expect(animeRepository.loadById).toHaveBeenCalledTimes(1)
+  })
+
+  it('should return NotFoundError if LoadAnimeById returns undefined', async () => {
+    animeRepository.loadById.mockResolvedValueOnce(undefined)
+
+    const promise = sut(makeAnime)
+
+    await expect(promise).rejects.toThrow(new NotFoundError('id'))
   })
 })
