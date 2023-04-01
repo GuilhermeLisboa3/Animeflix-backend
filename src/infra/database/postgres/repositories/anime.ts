b2eb33f77c59204893759be55cd2de672f27a2fc
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-04-01 18:46:17 -0300

    feat: ensure AnimeRepository return undefined on success

--- src/infra/database/postgres/repositories/anime.ts
+++ src/infra/database/postgres/repositories/anime.ts
@@ -1,7 +1,7 @@
-import { CheckAnime, CreateAnime, DeleteAnimeById, LoadAnimeById } from '@/domain/contracts/database/anime'
+import { CheckAnime, CreateAnime, DeleteAnimeById, LoadAnimeById, UpdateAnimeRepository } from '@/domain/contracts/database/anime'
 import { Anime } from '@/infra/database/postgres/entities'
 
-export class AnimeRepository implements CheckAnime, CreateAnime, LoadAnimeById, DeleteAnimeById {
+export class AnimeRepository implements CheckAnime, CreateAnime, LoadAnimeById, DeleteAnimeById, UpdateAnimeRepository {
   async check ({ name }: CheckAnime.Input): Promise<CheckAnime.Output> {
     const existAnime = await Anime.findOne({ where: { name } })
     return existAnime !== null
@@ -19,5 +19,10 @@ export class AnimeRepository implements CheckAnime, CreateAnime, LoadAnimeById, 
 
   async deleteById ({ id }: DeleteAnimeById.Input): Promise<void> {
     await Anime.destroy({ where: { id } })
+  }
+
+  async update ({ id, categoryId, featured, name, synopsis, thumbnailUrl }: UpdateAnimeRepository.Input): Promise<void> {
+    const attributes = { categoryId, featured, name, synopsis, thumbnailUrl }
+    await Anime.update(attributes, { where: { id } })
   }
 }
