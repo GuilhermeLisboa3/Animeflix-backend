f79c3c4ccf5fcdcaa548c03380a07bce4385a63f
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-04-01 19:17:56 -0300

    feat: ensure GetAnimeByIdUseCase return anime on success

--- src/domain/usecases/anime/get-anime-by-id.ts
+++ src/domain/usecases/anime/get-anime-by-id.ts
@@ -3,9 +3,11 @@ import { NotFoundError } from '@/domain/errors'
 
 type Setup = (animeRepository: LoadAnimeById) => GetAnimeById
 type Input = { id: string }
-export type GetAnimeById = (input: Input) => Promise<void>
+type Output = { id: number, name: string, thumbnailUrl: string, synopsis: string }
+export type GetAnimeById = (input: Input) => Promise<Output>
 
 export const GetAnimeByIdUseCase: Setup = (animeRepository) => async ({ id }) => {
   const anime = await animeRepository.loadById({ id })
   if (!anime) throw new NotFoundError('id')
+  return anime
 }
