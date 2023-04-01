68195807893f6553f7089d24fce566704a237e57
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-04-01 18:26:21 -0300

    feat: ensure UpdateAnimeUseCase return NotFoundError if LoadAnimeById returns undefined

--- src/domain/usecases/anime/update-anime.ts
+++ src/domain/usecases/anime/update-anime.ts
@@ -1,9 +1,11 @@
 import { LoadAnimeById } from '@/domain/contracts/database/anime'
+import { NotFoundError } from '@/domain/errors'
 
 type Setup = (animeRepository: LoadAnimeById) => UpdateAnime
 type Input = { id: string, name?: string, categoryId?: number, file?: { buffer: Buffer, mimeType: string }, synopsis?: string, featured?: boolean }
 export type UpdateAnime = (input: Input) => Promise<void>
 
 export const UpdateAnimeUseCase: Setup = (animeRepository) => async ({ id, file, categoryId, featured, name, synopsis }) => {
-  await animeRepository.loadById({ id })
+  const anime = await animeRepository.loadById({ id })
+  if (!anime) throw new NotFoundError('id')
 }
