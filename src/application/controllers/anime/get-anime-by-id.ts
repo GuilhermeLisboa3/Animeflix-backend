618fd70560074ea647d6d365f943ad9ee7acd415
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-04-01 19:39:05 -0300

    feat: ensure GetAnimeByIdController  extends Controller

--- src/application/controllers/anime/get-anime-by-id.ts
+++ src/application/controllers/anime/get-anime-by-id.ts
@@ -1,11 +1,12 @@
+import { Controller } from '@/application/controllers'
 import { HttpResponse, ok } from '@/application/helpers'
 import { Validator, ValidationBuilder as builder } from '@/application/validation'
 import { GetAnimeById } from '@/domain/usecases/anime'
 
 type HttpRequest = { id: string }
 
-export class GetAnimeByIdController {
-  constructor (private readonly getAnimeById: GetAnimeById) {}
+export class GetAnimeByIdController extends Controller {
+  constructor (private readonly getAnimeById: GetAnimeById) { super() }
 
   async perform ({ id }: HttpRequest): Promise<HttpResponse> {
     const anime = await this.getAnimeById({ id })
