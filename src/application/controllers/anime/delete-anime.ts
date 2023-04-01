b116e45fcf0dd9407a5868ada0d19ee1c06e6c0d
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-04-01 18:06:54 -0300

    feat: ensure DeleteAnimeController extends Controller

--- src/application/controllers/anime/delete-anime.ts
+++ src/application/controllers/anime/delete-anime.ts
@@ -1,11 +1,12 @@
+import { Controller } from '@/application/controllers'
 import { HttpResponse, noContent } from '@/application/helpers'
 import { Validator, ValidationBuilder as builder } from '@/application/validation'
 import { DeleteAnime } from '@/domain/usecases/anime'
 
 type HttpRequest = { id: string }
 
-export class DeleteAnimeController {
-  constructor (private readonly deleteAnime: DeleteAnime) {}
+export class DeleteAnimeController extends Controller {
+  constructor (private readonly deleteAnime: DeleteAnime) { super() }
 
   async perform ({ id }: HttpRequest): Promise<HttpResponse> {
     await this.deleteAnime({ id })
