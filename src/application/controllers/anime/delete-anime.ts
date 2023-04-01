cac8db020fb0d4b1579d800133d18edcc7567228
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-04-01 18:00:27 -0300

    feat: ensure DeleteAnimeController call DeleteAnime with correct input

--- src/application/controllers/anime/delete-anime.ts
+++ src/application/controllers/anime/delete-anime.ts
@@ -1,8 +1,15 @@
 import { Validator, ValidationBuilder as builder } from '@/application/validation'
+import { DeleteAnime } from '@/domain/usecases/anime'
 
 type HttpRequest = { id: string }
 
 export class DeleteAnimeController {
+  constructor (private readonly deleteAnime: DeleteAnime) {}
+
+  async perform ({ id }: HttpRequest): Promise<void> {
+    await this.deleteAnime({ id })
+  }
+
   buildValidators ({ id }: HttpRequest): Validator[] {
     return [
       ...builder.of(id, 'id').required().build()
