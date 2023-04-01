aa53afa2d9a394b8ea778ea21c22e28eca6842ce
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-04-01 20:37:33 -0300

    feat: ensure SearchAnimesByNameUseCase return list anime on success

--- src/domain/usecases/anime/search-animes-by-name.ts
+++ src/domain/usecases/anime/search-animes-by-name.ts
@@ -3,9 +3,10 @@ import { PaginationParams } from '@/domain/entities'
 
 type Setup = (animeRepository: ListAnimeByName) => SearchAnimesByName
 type Input = { page?: string, perPage?: string, name: string }
-export type SearchAnimesByName = (input: Input) => Promise<void>
+type Output = { animes: Array<{ id: number, name: string, thumbnailUrl: string, synopsis: string }>, page: number, perPage: number, count: number }
+export type SearchAnimesByName = (input: Input) => Promise<Output>
 
 export const SearchAnimesByNameUseCase: Setup = (animeRepository) => async ({ page, perPage, name }) => {
   const { pageNumber, perPageNumber } = new PaginationParams(page, perPage)
-  await animeRepository.listByName({ page: pageNumber, perPage: perPageNumber, name })
+  return await animeRepository.listByName({ page: pageNumber, perPage: perPageNumber, name })
 }
