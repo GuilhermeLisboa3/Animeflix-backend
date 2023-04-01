d90c1590f0773ac42f809a02eb50bcc762ccc2e4
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-04-01 19:34:18 -0300

    test: ensure GetAnimeByIdController call GetAnimeById with correct input

--- tests/application/controllers/anime/get-anime-by-id.spec.ts
+++ tests/application/controllers/anime/get-anime-by-id.spec.ts
@@ -4,13 +4,15 @@ import { RequiredField } from '@/application/validation'
 describe('GetAnimeByIdController', () => {
   let sut: GetAnimeByIdController
   let makeRequest: { id: string }
+  let GetAnimeById: jest.Mock
 
   beforeAll(() => {
     makeRequest = { id: '1' }
+    GetAnimeById = jest.fn()
   })
 
   beforeEach(() => {
-    sut = new GetAnimeByIdController()
+    sut = new GetAnimeByIdController(GetAnimeById)
   })
 
   it('should build Validators correctly', async () => {
@@ -19,5 +21,12 @@ describe('GetAnimeByIdController', () => {
     expect(validators).toEqual([
       new RequiredField('1', 'id')
     ])
+  })
+
+  it('should call GetAnimeById with correct input', async () => {
+    await sut.perform(makeRequest)
+
+    expect(GetAnimeById).toHaveBeenCalledWith({ id: '1' })
+    expect(GetAnimeById).toHaveBeenCalledTimes(1)
   })
 })
