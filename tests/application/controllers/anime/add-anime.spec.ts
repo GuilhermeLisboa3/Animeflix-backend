a39362cc0b7439842a1eae790cac9ea99b8ff740
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-04-01 12:34:55 -0300

    test: ensure AddAnimeController return 204 on success

--- tests/application/controllers/anime/add-anime.spec.ts
+++ tests/application/controllers/anime/add-anime.spec.ts
@@ -35,4 +35,13 @@ describe('AddAnimeController', () => {
     expect(AddAnime).toHaveBeenCalledWith(makeRequest)
     expect(AddAnime).toHaveBeenCalledTimes(1)
   })
+
+  it('should return 204 on success', async () => {
+    const httpResponse = await sut.perform(makeRequest)
+
+    expect(httpResponse).toEqual({
+      statusCode: 204,
+      data: null
+    })
+  })
 })
