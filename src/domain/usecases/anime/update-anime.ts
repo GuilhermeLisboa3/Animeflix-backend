411bfd2e79fe75d564f4718bef4933a1fabb4483
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-04-01 18:31:26 -0300

    feat: ensure UpdateAnimeUseCase call UUIDGenerator with correct input

--- src/domain/usecases/anime/update-anime.ts
+++ src/domain/usecases/anime/update-anime.ts
@@ -1,15 +1,16 @@
 import { LoadAnimeById } from '@/domain/contracts/database/anime'
-import { DeleteFile } from '@/domain/contracts/gateways'
+import { DeleteFile, UUIDGenerator } from '@/domain/contracts/gateways'
 import { NotFoundError } from '@/domain/errors'
 
-type Setup = (animeRepository: LoadAnimeById, fileStorage: DeleteFile) => UpdateAnime
+type Setup = (animeRepository: LoadAnimeById, fileStorage: DeleteFile, uuid: UUIDGenerator) => UpdateAnime
 type Input = { id: string, name?: string, categoryId?: number, file?: { buffer: Buffer, mimeType: string }, synopsis?: string, featured?: boolean }
 export type UpdateAnime = (input: Input) => Promise<void>
 
-export const UpdateAnimeUseCase: Setup = (animeRepository, fileStorage) => async ({ id, file, categoryId, featured, name, synopsis }) => {
+export const UpdateAnimeUseCase: Setup = (animeRepository, fileStorage, uuid) => async ({ id, file, categoryId, featured, name, synopsis }) => {
   const anime = await animeRepository.loadById({ id })
   if (!anime) throw new NotFoundError('id')
   if (file) {
     if (anime.thumbnailUrl) await fileStorage.delete({ fileName: anime.thumbnailUrl })
+    uuid.generate()
   }
 }
