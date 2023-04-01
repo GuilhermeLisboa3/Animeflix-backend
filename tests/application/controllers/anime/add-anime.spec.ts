6d93221e1d2d8cb13fa3002f9e87e5bbbee8a95e
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-04-01 12:37:48 -0300

    test: ensure AddAnimeController return 400 if AddAnime returns FieldInUseError

--- tests/application/controllers/anime/add-anime.spec.ts
+++ tests/application/controllers/anime/add-anime.spec.ts
@@ -1,6 +1,7 @@
 import { Controller } from '@/application/controllers'
 import { AddAnimeController } from '@/application/controllers/anime'
 import { RequiredField, AllowedMimeTypeValidation, MaxFileSizeValidation } from '@/application/validation'
+import { FieldInUseError } from '@/domain/errors'
 
 describe('AddAnimeController', () => {
   let sut: AddAnimeController
@@ -39,6 +40,16 @@ describe('AddAnimeController', () => {
 
     expect(AddAnime).toHaveBeenCalledWith(makeRequest)
     expect(AddAnime).toHaveBeenCalledTimes(1)
+  })
+
+  it('should return 400 if AddAnime returns FieldInUseError', async () => {
+    AddAnime.mockRejectedValueOnce(new FieldInUseError('name'))
+    const httpResponse = await sut.handle(makeRequest)
+
+    expect(httpResponse).toEqual({
+      statusCode: 400,
+      data: new FieldInUseError('name')
+    })
   })
 
   it('should return 204 on success', async () => {
