006bff99ff0501d42bfbb63f5f57b73ccddfb49b
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-04-01 18:36:00 -0300

    feat: ensure UpdateAnimeUseCase call CheckCategoryId with correct input

--- src/domain/usecases/anime/update-anime.ts
+++ src/domain/usecases/anime/update-anime.ts
@@ -1,17 +1,21 @@
 import { LoadAnimeById } from '@/domain/contracts/database/anime'
+import { CheckCategoryById } from '@/domain/contracts/database/category'
 import { DeleteFile, UUIDGenerator, UploadFile } from '@/domain/contracts/gateways'
 import { NotFoundError } from '@/domain/errors'
 
-type Setup = (animeRepository: LoadAnimeById, fileStorage: DeleteFile & UploadFile, uuid: UUIDGenerator) => UpdateAnime
+type Setup = (animeRepository: LoadAnimeById, fileStorage: DeleteFile & UploadFile, uuid: UUIDGenerator, categoryRepository: CheckCategoryById) => UpdateAnime
 type Input = { id: string, name?: string, categoryId?: number, file?: { buffer: Buffer, mimeType: string }, synopsis?: string, featured?: boolean }
 export type UpdateAnime = (input: Input) => Promise<void>
 
-export const UpdateAnimeUseCase: Setup = (animeRepository, fileStorage, uuid) => async ({ id, file, categoryId, featured, name, synopsis }) => {
+export const UpdateAnimeUseCase: Setup = (animeRepository, fileStorage, uuid, categoryRepository) => async ({ id, file, categoryId, featured, name, synopsis }) => {
   const anime = await animeRepository.loadById({ id })
   if (!anime) throw new NotFoundError('id')
   if (file) {
     if (anime.thumbnailUrl) await fileStorage.delete({ fileName: anime.thumbnailUrl })
     const key = uuid.generate()
     await fileStorage.upload({ file: file.buffer, fileName: `${key}.${file.mimeType.split('/')[1]}` })
+  }
+  if (categoryId) {
+    await categoryRepository.checkById({ id: categoryId })
   }
 }
