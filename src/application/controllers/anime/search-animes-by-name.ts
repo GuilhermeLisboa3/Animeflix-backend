a12cd094f64084b0c290b80dfc7721648171e756
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-04-01 21:17:58 -0300

    feat: ensure SearchAnimesByNameController call ListAnimeByName with correct input

--- src/application/controllers/anime/search-animes-by-name.ts
+++ src/application/controllers/anime/search-animes-by-name.ts
@@ -1,8 +1,15 @@
 import { Validator, ValidationBuilder as builder } from '@/application/validation'
+import { SearchAnimesByName } from '@/domain/usecases/anime'
 
 type HttpRequest = { name: string, page?: string, perPage?: string }
 
 export class SearchAnimesByNameController {
+  constructor (private readonly searchAnimesByName: SearchAnimesByName) {}
+
+  async perform ({ name, page, perPage }: HttpRequest): Promise<void> {
+    await this.searchAnimesByName({ name, page, perPage })
+  }
+
   buildValidators ({ name }: HttpRequest): Validator[] {
     return [
       ...builder.of(name, 'name').required().build()
