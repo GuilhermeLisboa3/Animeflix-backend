a0c621f627b0dc1f6201f9b06f203cada55d404b
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-04-01 21:19:41 -0300

    test: ensure SearchAnimesByNameController call ListAnimeByName with correct input

--- tests/application/controllers/anime/search-animes-by-name.spec.ts
+++ tests/application/controllers/anime/search-animes-by-name.spec.ts
@@ -4,13 +4,15 @@ import { RequiredField } from '@/application/validation'
 describe('SearchAnimesByNameController', () => {
   let sut: SearchAnimesByNameController
   let makeRequest: { name: string, page?: string, perPage?: string }
+  let ListAnimeByName: jest.Mock
 
   beforeAll(() => {
     makeRequest = { name: 'any_name' }
+    ListAnimeByName = jest.fn()
   })
 
   beforeEach(() => {
-    sut = new SearchAnimesByNameController()
+    sut = new SearchAnimesByNameController(ListAnimeByName)
   })
 
   it('should build Validators correctly', async () => {
@@ -19,5 +21,12 @@ describe('SearchAnimesByNameController', () => {
     expect(validators).toEqual([
       new RequiredField('any_name', 'name')
     ])
+  })
+
+  it('should call ListAnimeByName with correct input', async () => {
+    await sut.perform(makeRequest)
+
+    expect(ListAnimeByName).toHaveBeenCalledWith({ name: 'any_name' })
+    expect(ListAnimeByName).toHaveBeenCalledTimes(1)
   })
 })
