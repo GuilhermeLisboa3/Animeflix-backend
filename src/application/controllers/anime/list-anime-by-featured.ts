7f072293fa663ae2dedd86972bde52ed82778071
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-04-01 22:21:00 -0300

    feat: ensure ListAnimeByFeaturedController return 200 on success

--- src/application/controllers/anime/list-anime-by-featured.ts
+++ src/application/controllers/anime/list-anime-by-featured.ts
@@ -1,9 +1,11 @@
+import { HttpResponse, ok } from '@/application/helpers'
 import { ListAnimeFeatured } from '@/domain/usecases/anime'
 
 export class ListAnimeByFeaturedController {
   constructor (private readonly listAnimeFeatured: ListAnimeFeatured) {}
 
-  async perform (): Promise<void> {
-    await this.listAnimeFeatured()
+  async perform (): Promise<HttpResponse> {
+    const listAnimes = await this.listAnimeFeatured()
+    return ok(listAnimes)
   }
 }
