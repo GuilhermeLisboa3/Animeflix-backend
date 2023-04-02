a83b6925f9545715b368386f7d13b45ced551650
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-04-01 21:25:31 -0300

    feat: ensure SearchAnimesByNameController extends Controller

--- src/application/controllers/anime/search-animes-by-name.ts
+++ src/application/controllers/anime/search-animes-by-name.ts
@@ -1,11 +1,12 @@
+import { Controller } from '@/application/controllers'
 import { HttpResponse, ok } from '@/application/helpers'
 import { Validator, ValidationBuilder as builder } from '@/application/validation'
 import { SearchAnimesByName } from '@/domain/usecases/anime'
 
 type HttpRequest = { name: string, page?: string, perPage?: string }
 
-export class SearchAnimesByNameController {
-  constructor (private readonly searchAnimesByName: SearchAnimesByName) {}
+export class SearchAnimesByNameController extends Controller {
+  constructor (private readonly searchAnimesByName: SearchAnimesByName) { super() }
 
   async perform ({ name, page, perPage }: HttpRequest): Promise<HttpResponse> {
     const animes = await this.searchAnimesByName({ name, page, perPage })
