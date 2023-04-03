dcdf6019b1270b686f6a6c5045c4631c32995132
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-04-03 13:32:01 -0300

    feat: ensure DeleteEpisodeUseCase return NotFoundError if LoadEpisodeById returns undefined

--- src/domain/usecases/episode/delete-episode.ts
+++ src/domain/usecases/episode/delete-episode.ts
@@ -1,9 +1,11 @@
 import { LoadEpisodeById } from '@/domain/contracts/database/episode'
+import { NotFoundError } from '@/domain/errors'
 
 type Setup = (episodeRepository: LoadEpisodeById) => DeleteEpisode
 type Input = { episodeId: string }
 export type DeleteEpisode = (input: Input) => Promise<void>
 
 export const DeleteEpisodeUseCase: Setup = (episodeRepository) => async ({ episodeId }) => {
-  await episodeRepository.loadById({ id: episodeId })
+  const episode = await episodeRepository.loadById({ id: episodeId })
+  if (!episode) throw new NotFoundError('episodeId')
 }
