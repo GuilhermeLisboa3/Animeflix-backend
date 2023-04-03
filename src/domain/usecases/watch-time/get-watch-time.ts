90b84fcc949697b16ef81cd1778133d8e5c68236
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-04-03 16:20:32 -0300

    feat: ensure GetWatchTimeUseCase call CheckAccountById with correct input

---
+++ src/domain/usecases/watch-time/get-watch-time.ts
@@ -0,0 +1,9 @@
+import { CheckAccountById } from '@/domain/contracts/database/account'
+
+type Setup = (accountRepository: CheckAccountById) => GetWatchTime
+type Input = { accountId: string, episodeId: string }
+export type GetWatchTime = (input: Input) => Promise<void>
+
+export const GetWatchTimeUseCase: Setup = (accountRepository) => async ({ accountId, episodeId }) => {
+  await accountRepository.checkById({ id: Number(accountId) })
+}
