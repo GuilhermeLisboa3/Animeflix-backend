67acd8977845d7e94176768625f993c5399ada54
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-04-03 15:26:40 -0300

    feat: ensure AddWatchTimeUseCase return NotFoundError if CheckAccountById returns false

--- src/domain/usecases/watch-time/add-watch-time.ts
+++ src/domain/usecases/watch-time/add-watch-time.ts
@@ -1,9 +1,11 @@
 import { CheckAccountById } from '@/domain/contracts/database/account'
+import { NotFoundError } from '@/domain/errors'
 
 type Setup = (accountRepository: CheckAccountById) => AddWatchTime
 type Input = { accountId: number, episodeId: number, seconds: number }
 export type AddWatchTime = (input: Input) => Promise<void>
 
 export const AddWatchTimeUseCase: Setup = (accountRepository) => async ({ accountId, episodeId, seconds }) => {
-  await accountRepository.checkById({ id: accountId })
+  const existAccount = await accountRepository.checkById({ id: accountId })
+  if (!existAccount) throw new NotFoundError('accountId')
 }
