29e3716d771900b1a4eb611177893548a1fb6e4a
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-04-03 17:03:38 -0300

    feat: ensure GetWatchTimeController return 200 on success

--- src/application/controllers/watch-time/get-watch-time.ts
+++ src/application/controllers/watch-time/get-watch-time.ts
@@ -1,3 +1,4 @@
+import { HttpResponse, ok } from '@/application/helpers'
 import { Validator, ValidationBuilder as build } from '@/application/validation'
 import { GetWatchTime } from '@/domain/usecases/watch-time'
 
@@ -6,8 +7,9 @@ type HttpRequest = { accountId: string, id: string }
 export class GetWatchTimeController {
   constructor (private readonly getWatchTime: GetWatchTime) {}
 
-  async perform ({ accountId, id }: HttpRequest): Promise<void> {
-    await this.getWatchTime({ accountId, episodeId: id })
+  async perform ({ accountId, id }: HttpRequest): Promise<HttpResponse> {
+    const seconds = await this.getWatchTime({ accountId, episodeId: id })
+    return ok(seconds)
   }
 
   buildValidators ({ id }: HttpRequest): Validator[] {
