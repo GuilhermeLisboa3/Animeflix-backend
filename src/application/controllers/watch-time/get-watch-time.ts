0bd0ec4c17d3f37e527562ff3be29293e8d53be8
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-04-03 17:01:45 -0300

    feat: ensure GetWatchTimeController call GetWatchTime with correct input

--- src/application/controllers/watch-time/get-watch-time.ts
+++ src/application/controllers/watch-time/get-watch-time.ts
@@ -1,8 +1,15 @@
 import { Validator, ValidationBuilder as build } from '@/application/validation'
+import { GetWatchTime } from '@/domain/usecases/watch-time'
 
 type HttpRequest = { accountId: string, id: string }
 
 export class GetWatchTimeController {
+  constructor (private readonly getWatchTime: GetWatchTime) {}
+
+  async perform ({ accountId, id }: HttpRequest): Promise<void> {
+    await this.getWatchTime({ accountId, episodeId: id })
+  }
+
   buildValidators ({ id }: HttpRequest): Validator[] {
     return [
       ...build.of(id, 'id').required().build()
