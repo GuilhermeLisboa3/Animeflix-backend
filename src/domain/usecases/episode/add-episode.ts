5cd9ebd83a88d845c7d8b20683331a5ae776025c
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-04-03 10:37:18 -0300

    feat: ensure AddEpisodeUseCase return NotFoundError if CheckAnimeById returns false

--- src/domain/usecases/episode/add-episode.ts
+++ src/domain/usecases/episode/add-episode.ts
@@ -1,9 +1,11 @@
 import { CheckAnimeById } from '@/domain/contracts/database/anime'
+import { NotFoundError } from '@/domain/errors'
 
 type Setup = (animeRepository: CheckAnimeById) => AddEpisode
 type Input = { animeId: number, name: string, synopsis: string, secondsLong?: number, order: number, file?: { buffer: Buffer, mimeType: string } }
 export type AddEpisode = (input: Input) => Promise<void>
 
 export const AddEpisodeUseCase: Setup = (animeRepository) => async ({ animeId, order, file, name, synopsis, secondsLong }) => {
-  await animeRepository.checkById({ id: animeId })
+  const animeExists = await animeRepository.checkById({ id: animeId })
+  if (!animeExists) throw new NotFoundError('animeId')
 }
