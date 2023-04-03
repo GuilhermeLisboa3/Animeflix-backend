2976ce3ec15e850dfff1b724a17f9bc60c81befb
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-04-03 09:53:56 -0300

    feat: ensure ListAnimeNewestController extends Controller

--- src/application/controllers/anime/list-anime-newest.ts
+++ src/application/controllers/anime/list-anime-newest.ts
@@ -1,8 +1,9 @@
+import { Controller } from '@/application/controllers'
 import { HttpResponse, ok } from '@/application/helpers'
 import { ListAnimeNewest } from '@/domain/usecases/anime'
 
-export class ListAnimeNewestController {
-  constructor (private readonly listAnimeFeatured: ListAnimeNewest) {}
+export class ListAnimeNewestController extends Controller {
+  constructor (private readonly listAnimeFeatured: ListAnimeNewest) { super() }
 
   async perform (): Promise<HttpResponse> {
     const listAnime = await this.listAnimeFeatured()
