f73e6cb386d118fa00eb3ac33cd9ec56091b8680
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-04-03 12:55:11 -0300

    feat: ensure UpdateEpisodeUseCase call UploadFile with correct input

--- src/domain/usecases/episode/update-episode.ts
+++ src/domain/usecases/episode/update-episode.ts
@@ -1,8 +1,8 @@
 import { LoadEpisodeById, CheckEpisodeByOrder } from '@/domain/contracts/database/episode'
-import { DeleteFile, UUIDGenerator } from '@/domain/contracts/gateways'
+import { DeleteFile, UUIDGenerator, UploadFile } from '@/domain/contracts/gateways'
 import { FieldInUseError, NotFoundError } from '@/domain/errors'
 
-type Setup = (episodeRepository: LoadEpisodeById & CheckEpisodeByOrder, fileStorage: DeleteFile, uuid: UUIDGenerator) => UpdateEpisode
+type Setup = (episodeRepository: LoadEpisodeById & CheckEpisodeByOrder, fileStorage: DeleteFile & UploadFile, uuid: UUIDGenerator) => UpdateEpisode
 type Input = { id: string, animeId?: number, name?: string, synopsis?: string, secondsLong?: number, order?: number, file?: { buffer: Buffer, mimeType: string } }
 export type UpdateEpisode = (input: Input) => Promise<void>
 
@@ -15,6 +15,7 @@ export const UpdateEpisodeUseCase: Setup = (episodeRepository, fileStorage, uuid
   }
   if (file) {
     if (episode.videoUrl) await fileStorage.delete({ fileName: episode.videoUrl })
-    uuid.generate()
+    const key = uuid.generate()
+    await fileStorage.upload({ file: file.buffer, fileName: `${key}.${file.mimeType.split('/')[1]}` })
   }
 }
