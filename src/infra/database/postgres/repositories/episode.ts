f828f9a6d685871c3bec9abcf82e3a0e84e36e0a
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-04-03 11:12:20 -0300

    feat: ensure EpisodeRepository return true if the episode is successfully created

--- src/infra/database/postgres/repositories/episode.ts
+++ src/infra/database/postgres/repositories/episode.ts
@@ -1,9 +1,14 @@
-import { CheckEpisodeByOrder } from '@/domain/contracts/database/episode'
+import { CheckEpisodeByOrder, CreateEpisode } from '@/domain/contracts/database/episode'
 import { Episode } from '@/infra/database/postgres/entities'
 
-export class EpisodeRepository implements CheckEpisodeByOrder {
+export class EpisodeRepository implements CheckEpisodeByOrder, CreateEpisode {
   async checkByOrder ({ order }: CheckEpisodeByOrder.Input): Promise<CheckEpisodeByOrder.Output> {
     const existEpisode = await Episode.findOne({ where: { order } })
     return existEpisode !== null
+  }
+
+  async create ({ name, animeId, order, synopsis, secondsLong, videoUrl }: CreateEpisode.Input): Promise<CreateEpisode.Output> {
+    const episode = await Episode.create({ name, animeId, order, synopsis, secondsLong, videoUrl })
+    return episode !== null
   }
 }
