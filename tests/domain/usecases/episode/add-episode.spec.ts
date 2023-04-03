abecd987598fef5310eaea6810d6d9b9508dd917
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-04-03 10:42:36 -0300

    test: ensure AddEpisodeUseCase call CheckEpisodeByOrder with correct input

--- tests/domain/usecases/episode/add-episode.spec.ts
+++ tests/domain/usecases/episode/add-episode.spec.ts
@@ -1,11 +1,13 @@
 import { AddEpisodeUseCase, AddEpisode } from '@/domain/usecases/episode'
 import { CheckAnimeById } from '@/domain/contracts/database/anime'
+import { CheckEpisodeByOrder } from '@/domain/contracts/database/episode'
 
 import { mock, MockProxy } from 'jest-mock-extended'
 import { NotFoundError } from '@/domain/errors'
 
 describe('AddEpisodeUseCase', () => {
   let animeRepository: MockProxy<CheckAnimeById>
+  let episodeRepository: MockProxy<CheckEpisodeByOrder>
   let makeEpisode: { animeId: number, order: number, name: string, synopsis: string, file?: { buffer: Buffer, mimeType: string } }
   let sut: AddEpisode
 
@@ -13,10 +15,11 @@ describe('AddEpisodeUseCase', () => {
     makeEpisode = { animeId: 1, name: 'any_name', order: 1, synopsis: 'any_synopsis', file: { buffer: Buffer.from('any'), mimeType: 'video/mp4' } }
     animeRepository = mock()
     animeRepository.checkById.mockResolvedValue(true)
+    episodeRepository = mock()
   })
 
   beforeEach(() => {
-    sut = AddEpisodeUseCase(animeRepository)
+    sut = AddEpisodeUseCase(animeRepository, episodeRepository)
   })
 
   it('should call CheckAnimeById with correct input', async () => {
@@ -32,5 +35,12 @@ describe('AddEpisodeUseCase', () => {
     const promise = sut(makeEpisode)
 
     await expect(promise).rejects.toThrow(new NotFoundError('animeId'))
+  })
+
+  it('should call CheckEpisodeByOrder with correct input', async () => {
+    await sut(makeEpisode)
+
+    expect(episodeRepository.checkByOrder).toHaveBeenCalledWith({ order: 1 })
+    expect(episodeRepository.checkByOrder).toHaveBeenCalledTimes(1)
   })
 })
