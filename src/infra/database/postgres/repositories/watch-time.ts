e5d7d0288a0a5c583f02939b4f601b93fa645fd0
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-04-03 16:47:20 -0300

    feat: ensure WatchTimeRepository return seconds on success

--- src/infra/database/postgres/repositories/watch-time.ts
+++ src/infra/database/postgres/repositories/watch-time.ts
@@ -1,7 +1,7 @@
-import { SaveWatchTime } from '@/domain/contracts/database/watch-time'
+import { LoadWatchTime, SaveWatchTime } from '@/domain/contracts/database/watch-time'
 import { WatchTime } from '@/infra/database/postgres/entities'
 
-export class WatchTimeRepository implements SaveWatchTime {
+export class WatchTimeRepository implements SaveWatchTime, LoadWatchTime {
   async save ({ episodeId, seconds, userId }: SaveWatchTime.Input): Promise<void> {
     const watchTimeAlreadyExists = await WatchTime.findOne({ where: { userId, episodeId } })
     if (watchTimeAlreadyExists) {
@@ -10,5 +10,10 @@ export class WatchTimeRepository implements SaveWatchTime {
     } else {
       await WatchTime.create({ userId, episodeId, seconds })
     }
+  }
+
+  async load ({ episodeId, userId }: LoadWatchTime.Input): Promise<LoadWatchTime.Output> {
+    const watchTime = await WatchTime.findOne({ where: { episodeId, userId } })
+    return watchTime !== null ? { seconds: watchTime.seconds } : { seconds: 0 }
   }
 }
