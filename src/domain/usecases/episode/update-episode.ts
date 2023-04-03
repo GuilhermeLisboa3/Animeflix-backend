645586e8b30af69766191ed9932c6be4c4c9851d
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-04-03 12:45:52 -0300

    feat: ensure UpdateEpisodeUseCase call CheckEpisodeByOrder with correct input

--- src/domain/usecases/episode/update-episode.ts
+++ src/domain/usecases/episode/update-episode.ts
@@ -1,11 +1,14 @@
-import { LoadEpisodeById } from '@/domain/contracts/database/episode'
+import { LoadEpisodeById, CheckEpisodeByOrder } from '@/domain/contracts/database/episode'
 import { NotFoundError } from '@/domain/errors'
 
-type Setup = (episodeRepository: LoadEpisodeById) => UpdateEpisode
+type Setup = (episodeRepository: LoadEpisodeById & CheckEpisodeByOrder) => UpdateEpisode
 type Input = { id: string, animeId?: number, name?: string, synopsis?: string, secondsLong?: number, order?: number, file?: { buffer: Buffer, mimeType: string } }
 export type UpdateEpisode = (input: Input) => Promise<void>
 
 export const UpdateEpisodeUseCase: Setup = (episodeRepository) => async ({ id, animeId, order, file, name, synopsis, secondsLong }) => {
   const animeExists = await episodeRepository.loadById({ id })
   if (!animeExists) throw new NotFoundError('id')
+  if (order) {
+    await episodeRepository.checkByOrder({ order })
+  }
 }
