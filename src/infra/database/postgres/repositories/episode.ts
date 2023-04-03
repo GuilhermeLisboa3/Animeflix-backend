912080e8bff6e91ec28b6a4c029dd2592f3c4ea5
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-04-03 13:07:05 -0300

    feat: ensure EpisodeRepository return episode on success

--- src/infra/database/postgres/repositories/episode.ts
+++ src/infra/database/postgres/repositories/episode.ts
@@ -1,7 +1,7 @@
-import { CheckEpisodeByOrder, CreateEpisode } from '@/domain/contracts/database/episode'
+import { CheckEpisodeByOrder, CreateEpisode, LoadEpisodeById } from '@/domain/contracts/database/episode'
 import { Episode } from '@/infra/database/postgres/entities'
 
-export class EpisodeRepository implements CheckEpisodeByOrder, CreateEpisode {
+export class EpisodeRepository implements CheckEpisodeByOrder, CreateEpisode, LoadEpisodeById {
   async checkByOrder ({ order }: CheckEpisodeByOrder.Input): Promise<CheckEpisodeByOrder.Output> {
     const existEpisode = await Episode.findOne({ where: { order } })
     return existEpisode !== null
@@ -10,5 +10,10 @@ export class EpisodeRepository implements CheckEpisodeByOrder, CreateEpisode {
   async create ({ name, animeId, order, synopsis, secondsLong, videoUrl }: CreateEpisode.Input): Promise<CreateEpisode.Output> {
     const episode = await Episode.create({ name, animeId, order, synopsis, secondsLong, videoUrl })
     return episode !== null
+  }
+
+  async loadById ({ id }: LoadEpisodeById.Input): Promise<LoadEpisodeById.Output> {
+    const episode = await Episode.findOne({ attributes: [['video_url', 'videoUrl']], where: { id } })
+    return episode !== null ? episode : undefined
   }
 }
