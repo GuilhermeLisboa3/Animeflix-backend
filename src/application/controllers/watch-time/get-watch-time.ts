d60a5c7b908475812d513d9b7060c8290c50b142
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-04-03 17:05:33 -0300

    feat: ensure GetWatchTimeController extends Controller

--- src/application/controllers/watch-time/get-watch-time.ts
+++ src/application/controllers/watch-time/get-watch-time.ts
@@ -1,18 +1,19 @@
+import { Controller } from '@/application/controllers'
 import { HttpResponse, ok } from '@/application/helpers'
 import { Validator, ValidationBuilder as build } from '@/application/validation'
 import { GetWatchTime } from '@/domain/usecases/watch-time'
 
 type HttpRequest = { accountId: string, id: string }
 
-export class GetWatchTimeController {
-  constructor (private readonly getWatchTime: GetWatchTime) {}
+export class GetWatchTimeController extends Controller {
+  constructor (private readonly getWatchTime: GetWatchTime) { super() }
 
   async perform ({ accountId, id }: HttpRequest): Promise<HttpResponse> {
     const seconds = await this.getWatchTime({ accountId, episodeId: id })
     return ok(seconds)
   }
 
-  buildValidators ({ id }: HttpRequest): Validator[] {
+  override buildValidators ({ id }: HttpRequest): Validator[] {
     return [
       ...build.of(id, 'id').required().build()
     ]
