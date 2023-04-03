a208bb16fe28c7403a8dbabe7e4707711e784df6
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-04-03 09:52:57 -0300

    test: ensure ListAnimeNewestController return 200 on success

--- tests/application/controllers/anime/list-anime-newest.spec.ts
+++ tests/application/controllers/anime/list-anime-newest.spec.ts
@@ -5,7 +5,7 @@ describe('ListAnimeNewestController', () => {
   let ListAnimeNewest: jest.Mock
 
   beforeAll(() => {
-    ListAnimeNewest = jest.fn()
+    ListAnimeNewest = jest.fn().mockResolvedValue([{ id: 1, name: 'any_name', categoryId: 1, synopsis: 'any_synopsis', featured: false, thumbnailUrl: null }])
   })
 
   beforeEach(() => {
@@ -17,5 +17,14 @@ describe('ListAnimeNewestController', () => {
 
     expect(ListAnimeNewest).toHaveBeenCalledWith()
     expect(ListAnimeNewest).toHaveBeenCalledTimes(1)
+  })
+
+  it('should return 200 on success', async () => {
+    const httpResponse = await sut.perform()
+
+    expect(httpResponse).toEqual({
+      statusCode: 200,
+      data: [{ id: 1, name: 'any_name', categoryId: 1, synopsis: 'any_synopsis', featured: false, thumbnailUrl: null }]
+    })
   })
 })
