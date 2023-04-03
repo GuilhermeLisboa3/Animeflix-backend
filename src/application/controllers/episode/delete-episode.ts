a76a6c151bdf5afb7fa4f40082746f5db54fd3b9
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-04-03 13:49:48 -0300

    feat: ensure DeleteEpisodeController extends Controller

--- src/application/controllers/episode/delete-episode.ts
+++ src/application/controllers/episode/delete-episode.ts
@@ -1,10 +1,11 @@
+import { Controller } from '@/application/controllers'
 import { HttpResponse, noContent } from '@/application/helpers'
 import { DeleteEpisode } from '@/domain/usecases/episode'
 
 type HttpRequest = { id: string }
 
-export class DeleteEpisodeController {
-  constructor (private readonly deleteEpisode: DeleteEpisode) {}
+export class DeleteEpisodeController extends Controller {
+  constructor (private readonly deleteEpisode: DeleteEpisode) { super() }
 
   async perform ({ id }: HttpRequest): Promise<HttpResponse> {
     await this.deleteEpisode({ episodeId: id })
