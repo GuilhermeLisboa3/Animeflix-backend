bf98c7d7954c7f373da8db57d0611d8638ebe2b3
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-04-03 16:00:12 -0300

    test: ensure AddWatchTimeController call AddWatchTime with correct input

--- tests/application/controllers/watch-time/add-watch-time.spec.ts
+++ tests/application/controllers/watch-time/add-watch-time.spec.ts
@@ -4,13 +4,15 @@ import { RequiredField } from '@/application/validation'
 describe('AddWatchTimeController', () => {
   let makeRequest: { accountId: string, id: string, seconds: number }
   let sut: AddWatchTimeController
+  let AddWatchTime: jest.Mock
 
   beforeAll(() => {
     makeRequest = { accountId: '1', id: '1', seconds: 1 }
+    AddWatchTime = jest.fn()
   })
 
   beforeEach(() => {
-    sut = new AddWatchTimeController()
+    sut = new AddWatchTimeController(AddWatchTime)
   })
 
   it('should build Validators correctly', async () => {
@@ -20,5 +22,12 @@ describe('AddWatchTimeController', () => {
       new RequiredField('1', 'id'),
       new RequiredField(1, 'seconds')
     ])
+  })
+
+  it('should call AddWatchTime with correct input', async () => {
+    await sut.perform(makeRequest)
+
+    expect(AddWatchTime).toHaveBeenCalledWith({ accountId: 1, episodeId: 1, seconds: 1 })
+    expect(AddWatchTime).toHaveBeenCalledTimes(1)
   })
 })
