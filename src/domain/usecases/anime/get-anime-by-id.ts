3de0f9e08c4097f2a27aba58ec81417e740c04eb
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-04-03 14:27:00 -0300

    feat: ensure GetAnimeByIdUseCase return anime on success

--- src/domain/usecases/anime/get-anime-by-id.ts
+++ src/domain/usecases/anime/get-anime-by-id.ts
@@ -4,12 +4,18 @@ import { NotFoundError } from '@/domain/errors'
 
 type Setup = (animeRepository: LoadAnimeById, episodeRepository: LoadEpisodeByAnimeId) => GetAnimeById
 type Input = { id: string }
-type Output = { id: number, name: string, thumbnailUrl: string, synopsis: string }
+type Output = { id: number, name: string, thumbnailUrl: string, synopsis: string, episodes: Array<{ id: number, name: string, videoUrl: string, synopsis: string, secondsLong: number, order: number }> }
 export type GetAnimeById = (input: Input) => Promise<Output>
 
 export const GetAnimeByIdUseCase: Setup = (animeRepository, episodeRepository) => async ({ id }) => {
   const anime = await animeRepository.loadById({ id })
   if (!anime) throw new NotFoundError('id')
-  await episodeRepository.loadByAnimeId({ animeId: id })
-  return anime
+  const episodes = await episodeRepository.loadByAnimeId({ animeId: id })
+  return {
+    id: anime.id,
+    name: anime.name,
+    thumbnailUrl: anime.thumbnailUrl,
+    synopsis: anime.synopsis,
+    episodes
+  }
 }
