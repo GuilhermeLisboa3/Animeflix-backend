510ac39351250b03db695bc365f8e9bccaa27f9c
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-04-03 16:23:26 -0300

    test: ensure GetWatchTimeUseCase return NotFoundError if CheckAccountById returns false

--- tests/domain/usecases/watch-time/get-watch-time.spec.ts
+++ tests/domain/usecases/watch-time/get-watch-time.spec.ts
@@ -2,6 +2,7 @@ import { GetWatchTimeUseCase, GetWatchTime } from '@/domain/usecases/watch-time'
 import { CheckAccountById } from '@/domain/contracts/database/account'
 
 import { MockProxy, mock } from 'jest-mock-extended'
+import { NotFoundError } from '@/domain/errors'
 
 describe('GetWatchTimeUseCase', () => {
   let accountRepository: MockProxy<CheckAccountById>
@@ -10,6 +11,7 @@ describe('GetWatchTimeUseCase', () => {
 
   beforeAll(() => {
     accountRepository = mock()
+    accountRepository.checkById.mockResolvedValue(true)
     makeWatchTime = { accountId: '1', episodeId: '1' }
   })
 
@@ -22,5 +24,13 @@ describe('GetWatchTimeUseCase', () => {
 
     expect(accountRepository.checkById).toHaveBeenCalledWith({ id: 1 })
     expect(accountRepository.checkById).toHaveBeenCalledTimes(1)
+  })
+
+  it('should return NotFoundError if CheckAccountById returns false', async () => {
+    accountRepository.checkById.mockResolvedValueOnce(false)
+
+    const promise = sut(makeWatchTime)
+
+    await expect(promise).rejects.toThrow(new NotFoundError('accountId'))
   })
 })
