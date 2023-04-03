03930106d1d6b92bde8d5f9ef7b52455dd80fbab
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-04-03 16:03:51 -0300

    feat: ensure AddWatchTimeController extends Controller

--- src/application/controllers/watch-time/add-watch-time.ts
+++ src/application/controllers/watch-time/add-watch-time.ts
@@ -1,11 +1,12 @@
+import { Controller } from '@/application/controllers'
 import { HttpResponse, noContent } from '@/application/helpers'
 import { Validator, ValidationBuilder as build } from '@/application/validation'
 import { AddWatchTime } from '@/domain/usecases/watch-time'
 
 type HttpRequest = { accountId: string, id: string, seconds: number }
 
-export class AddWatchTimeController {
-  constructor (private readonly addWatchTime: AddWatchTime) {}
+export class AddWatchTimeController extends Controller {
+  constructor (private readonly addWatchTime: AddWatchTime) { super() }
 
   async perform ({ accountId, id, seconds }: HttpRequest): Promise<HttpResponse> {
     await this.addWatchTime({ accountId: Number(accountId), episodeId: Number(id), seconds })
