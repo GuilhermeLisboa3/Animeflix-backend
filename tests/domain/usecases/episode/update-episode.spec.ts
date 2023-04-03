c940e9e94b5d360d8aa80a0e3360b741d7268214
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-04-03 12:44:57 -0300

    test: ensure UpdateEpisodeUseCase return NotFoundError if LoadEpisodeById returns undefined

--- tests/domain/usecases/episode/update-episode.spec.ts
+++ tests/domain/usecases/episode/update-episode.spec.ts
@@ -2,6 +2,7 @@ import { UpdateEpisodeUseCase, UpdateEpisode } from '@/domain/usecases/episode'
 import { LoadEpisodeById } from '@/domain/contracts/database/episode'
 
 import { MockProxy, mock } from 'jest-mock-extended'
+import { NotFoundError } from '@/domain/errors'
 
 describe('UpdateEpisodeUseCase', () => {
   let episodeRepository: MockProxy<LoadEpisodeById>
@@ -11,6 +12,7 @@ describe('UpdateEpisodeUseCase', () => {
   beforeAll(() => {
     makeEpisode = { id: '1', file: { buffer: Buffer.from('any'), mimeType: 'image/mp4' }, animeId: 1, order: 1 }
     episodeRepository = mock()
+    episodeRepository.loadById.mockResolvedValue({ videoUrl: 'any_value' })
   })
 
   beforeEach(() => {
@@ -22,5 +24,13 @@ describe('UpdateEpisodeUseCase', () => {
 
     expect(episodeRepository.loadById).toHaveBeenCalledWith({ id: '1' })
     expect(episodeRepository.loadById).toHaveBeenCalledTimes(1)
+  })
+
+  it('should return NotFoundError if LoadEpisodeById returns undefined', async () => {
+    episodeRepository.loadById.mockResolvedValueOnce(undefined)
+
+    const promise = sut(makeEpisode)
+
+    await expect(promise).rejects.toThrow(new NotFoundError('id'))
   })
 })
