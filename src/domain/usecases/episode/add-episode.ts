671e5430ac1c4a73c612ac6a79bc1031da618a7c
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-04-03 10:48:55 -0300

    feat: ensure AddEpisodeUseCase call UUIDGenerator with correct input

--- src/domain/usecases/episode/add-episode.ts
+++ src/domain/usecases/episode/add-episode.ts
@@ -1,14 +1,16 @@
 import { CheckAnimeById } from '@/domain/contracts/database/anime'
 import { CheckEpisodeByOrder } from '@/domain/contracts/database/episode'
+import { UUIDGenerator } from '@/domain/contracts/gateways'
 import { FieldInUseError, NotFoundError } from '@/domain/errors'
 
-type Setup = (animeRepository: CheckAnimeById, episodeRepository: CheckEpisodeByOrder) => AddEpisode
+type Setup = (animeRepository: CheckAnimeById, episodeRepository: CheckEpisodeByOrder, uuid: UUIDGenerator) => AddEpisode
 type Input = { animeId: number, name: string, synopsis: string, secondsLong?: number, order: number, file?: { buffer: Buffer, mimeType: string } }
 export type AddEpisode = (input: Input) => Promise<void>
 
-export const AddEpisodeUseCase: Setup = (animeRepository, episodeRepository) => async ({ animeId, order, file, name, synopsis, secondsLong }) => {
+export const AddEpisodeUseCase: Setup = (animeRepository, episodeRepository, uuid) => async ({ animeId, order, file, name, synopsis, secondsLong }) => {
   const animeExists = await animeRepository.checkById({ id: animeId })
   if (!animeExists) throw new NotFoundError('animeId')
   const episodeExists = await episodeRepository.checkByOrder({ order })
   if (episodeExists) throw new FieldInUseError('order')
+  uuid.generate()
 }
