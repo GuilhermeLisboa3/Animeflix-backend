67ea9453a421e1c69f03703abe79fa1954f47b6c
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-04-03 13:48:01 -0300

    feat: ensure DeleteEpisodeController return 204 on success

--- src/application/controllers/episode/delete-episode.ts
+++ src/application/controllers/episode/delete-episode.ts
@@ -1,3 +1,4 @@
+import { HttpResponse, noContent } from '@/application/helpers'
 import { DeleteEpisode } from '@/domain/usecases/episode'
 
 type HttpRequest = { id: string }
@@ -5,7 +6,8 @@ type HttpRequest = { id: string }
 export class DeleteEpisodeController {
   constructor (private readonly deleteEpisode: DeleteEpisode) {}
 
-  async perform ({ id }: HttpRequest): Promise<void> {
+  async perform ({ id }: HttpRequest): Promise<HttpResponse> {
     await this.deleteEpisode({ episodeId: id })
+    return noContent()
   }
 }
