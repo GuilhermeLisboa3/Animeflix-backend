19b83902c33c7fd22e30b3da16816a86454d460f
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-04-03 10:43:32 -0300

    feat: ensure AddEpisodeUseCase throw FieldInUseError if CheckEpisodeByOrder return true

--- src/domain/usecases/episode/add-episode.ts
+++ src/domain/usecases/episode/add-episode.ts
@@ -1,6 +1,6 @@
 import { CheckAnimeById } from '@/domain/contracts/database/anime'
 import { CheckEpisodeByOrder } from '@/domain/contracts/database/episode'
-import { NotFoundError } from '@/domain/errors'
+import { FieldInUseError, NotFoundError } from '@/domain/errors'
 
 type Setup = (animeRepository: CheckAnimeById, episodeRepository: CheckEpisodeByOrder) => AddEpisode
 type Input = { animeId: number, name: string, synopsis: string, secondsLong?: number, order: number, file?: { buffer: Buffer, mimeType: string } }
@@ -9,5 +9,6 @@ export type AddEpisode = (input: Input) => Promise<void>
 export const AddEpisodeUseCase: Setup = (animeRepository, episodeRepository) => async ({ animeId, order, file, name, synopsis, secondsLong }) => {
   const animeExists = await animeRepository.checkById({ id: animeId })
   if (!animeExists) throw new NotFoundError('animeId')
-  await episodeRepository.checkByOrder({ order })
+  const episodeExists = await episodeRepository.checkByOrder({ order })
+  if (episodeExists) throw new FieldInUseError('order')
 }
