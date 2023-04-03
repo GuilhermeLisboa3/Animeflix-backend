793028ba7e37f990b084bdca56e52d75a48199bd
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-04-03 14:08:08 -0300

    test: ensure StreamEpisodeController call StreamEpisode with correct input

--- tests/application/controllers/episode/stream-episode.spec.ts
+++ tests/application/controllers/episode/stream-episode.spec.ts
@@ -4,13 +4,15 @@ import { RequiredField } from '@/application/validation'
 describe('StreamEpisodeController', () => {
   let sut: StreamEpisodeController
   let makeRequest: { animeId: string, order: string }
+  let StreamEpisode: jest.Mock
 
   beforeAll(() => {
     makeRequest = { animeId: '1', order: '1' }
+    StreamEpisode = jest.fn()
   })
 
   beforeEach(() => {
-    sut = new StreamEpisodeController()
+    sut = new StreamEpisodeController(StreamEpisode)
   })
 
   it('should build Validators correctly', async () => {
@@ -20,5 +22,12 @@ describe('StreamEpisodeController', () => {
       new RequiredField('1', 'order'),
       new RequiredField('1', 'animeId')
     ])
+  })
+
+  it('should call StreamEpisode with correct input', async () => {
+    await sut.perform(makeRequest)
+
+    expect(StreamEpisode).toHaveBeenCalledWith(makeRequest)
+    expect(StreamEpisode).toHaveBeenCalledTimes(1)
   })
 })
