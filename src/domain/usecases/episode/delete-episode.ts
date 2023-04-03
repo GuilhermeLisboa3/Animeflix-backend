593a7b3b7f81ef7f1581b9708e418184da436e3f
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-04-03 13:34:01 -0300

    feat: ensure DeleteEpisodeUseCase call DeleteFile with correct input

--- src/domain/usecases/episode/delete-episode.ts
+++ src/domain/usecases/episode/delete-episode.ts
@@ -1,11 +1,15 @@
 import { LoadEpisodeById } from '@/domain/contracts/database/episode'
+import { DeleteFile } from '@/domain/contracts/gateways'
 import { NotFoundError } from '@/domain/errors'
 
-type Setup = (episodeRepository: LoadEpisodeById) => DeleteEpisode
+type Setup = (episodeRepository: LoadEpisodeById, fileStorage: DeleteFile) => DeleteEpisode
 type Input = { episodeId: string }
 export type DeleteEpisode = (input: Input) => Promise<void>
 
-export const DeleteEpisodeUseCase: Setup = (episodeRepository) => async ({ episodeId }) => {
+export const DeleteEpisodeUseCase: Setup = (episodeRepository, fileStorage) => async ({ episodeId }) => {
   const episode = await episodeRepository.loadById({ id: episodeId })
   if (!episode) throw new NotFoundError('episodeId')
+  if (episode.videoUrl !== null) {
+    await fileStorage.delete({ fileName: episode.videoUrl })
+  }
 }
