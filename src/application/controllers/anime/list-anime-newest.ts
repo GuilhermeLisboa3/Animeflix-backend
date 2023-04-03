626ace81d3c231907a43d407e0246e0f6a3686d4
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-04-03 09:52:03 -0300

    feat: ensure ListAnimeNewestController return 200 on success

--- src/application/controllers/anime/list-anime-newest.ts
+++ src/application/controllers/anime/list-anime-newest.ts
@@ -1,9 +1,11 @@
+import { HttpResponse, ok } from '@/application/helpers'
 import { ListAnimeNewest } from '@/domain/usecases/anime'
 
 export class ListAnimeNewestController {
   constructor (private readonly listAnimeFeatured: ListAnimeNewest) {}
 
-  async perform (): Promise<void> {
-    await this.listAnimeFeatured()
+  async perform (): Promise<HttpResponse> {
+    const listAnime = await this.listAnimeFeatured()
+    return ok(listAnime)
   }
 }
