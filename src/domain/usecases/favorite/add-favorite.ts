277c9a5125a32706f4efd27689a376e423526122
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-04-03 20:25:50 -0300

    feat: ensure AddFavoriteUseCase call CheckAnimeById with correct input

--- src/domain/usecases/favorite/add-favorite.ts
+++ src/domain/usecases/favorite/add-favorite.ts
@@ -1,11 +1,13 @@
 import { CheckAccountById } from '@/domain/contracts/database/account'
+import { CheckAnimeById } from '@/domain/contracts/database/anime'
 import { NotFoundError } from '@/domain/errors'
 
-type Setup = (accountRepository: CheckAccountById) => AddFavorite
+type Setup = (accountRepository: CheckAccountById, animeRepository: CheckAnimeById) => AddFavorite
 type Input = { accountId: string, animeId: number}
 export type AddFavorite = (input: Input) => Promise<void>
 
-export const AddFavoriteUseCase: Setup = (accountRepository) => async ({ accountId, animeId }) => {
+export const AddFavoriteUseCase: Setup = (accountRepository, animeRepository) => async ({ accountId, animeId }) => {
   const existAccount = await accountRepository.checkById({ id: Number(accountId) })
   if (!existAccount) throw new NotFoundError('accountId')
+  await animeRepository.checkById({ id: animeId })
 }
