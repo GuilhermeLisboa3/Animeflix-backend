ac1ab4a2ee3c0741c3b05142f40fa60b735d6ba3
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-04-03 12:51:09 -0300

    feat: ensure UpdateEpisodeUseCase call DeleteFile with correct input

--- src/domain/usecases/episode/update-episode.ts
+++ src/domain/usecases/episode/update-episode.ts
@@ -1,15 +1,19 @@
 import { LoadEpisodeById, CheckEpisodeByOrder } from '@/domain/contracts/database/episode'
+import { DeleteFile } from '@/domain/contracts/gateways'
 import { FieldInUseError, NotFoundError } from '@/domain/errors'
 
-type Setup = (episodeRepository: LoadEpisodeById & CheckEpisodeByOrder) => UpdateEpisode
+type Setup = (episodeRepository: LoadEpisodeById & CheckEpisodeByOrder, fileStorage: DeleteFile) => UpdateEpisode
 type Input = { id: string, animeId?: number, name?: string, synopsis?: string, secondsLong?: number, order?: number, file?: { buffer: Buffer, mimeType: string } }
 export type UpdateEpisode = (input: Input) => Promise<void>
 
-export const UpdateEpisodeUseCase: Setup = (episodeRepository) => async ({ id, animeId, order, file, name, synopsis, secondsLong }) => {
-  const animeExists = await episodeRepository.loadById({ id })
-  if (!animeExists) throw new NotFoundError('id')
+export const UpdateEpisodeUseCase: Setup = (episodeRepository, fileStorage) => async ({ id, animeId, order, file, name, synopsis, secondsLong }) => {
+  const episode = await episodeRepository.loadById({ id })
+  if (!episode) throw new NotFoundError('id')
   if (order) {
     const existsEpisode = await episodeRepository.checkByOrder({ order })
     if (existsEpisode) throw new FieldInUseError('order')
+  }
+  if (file) {
+    if (episode.videoUrl) await fileStorage.delete({ fileName: episode.videoUrl })
   }
 }
