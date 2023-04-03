74f473636e2198e57bcf74b70205b46e3ad1940e
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-04-03 12:57:23 -0300

    feat: ensure UpdateEpisodeUseCase call CheckAnimeById with correct input

--- src/domain/usecases/episode/update-episode.ts
+++ src/domain/usecases/episode/update-episode.ts
@@ -1,12 +1,13 @@
+import { CheckAnimeById } from '@/domain/contracts/database/anime'
 import { LoadEpisodeById, CheckEpisodeByOrder } from '@/domain/contracts/database/episode'
 import { DeleteFile, UUIDGenerator, UploadFile } from '@/domain/contracts/gateways'
 import { FieldInUseError, NotFoundError } from '@/domain/errors'
 
-type Setup = (episodeRepository: LoadEpisodeById & CheckEpisodeByOrder, fileStorage: DeleteFile & UploadFile, uuid: UUIDGenerator) => UpdateEpisode
+type Setup = (episodeRepository: LoadEpisodeById & CheckEpisodeByOrder, fileStorage: DeleteFile & UploadFile, uuid: UUIDGenerator, animeRepository: CheckAnimeById) => UpdateEpisode
 type Input = { id: string, animeId?: number, name?: string, synopsis?: string, secondsLong?: number, order?: number, file?: { buffer: Buffer, mimeType: string } }
 export type UpdateEpisode = (input: Input) => Promise<void>
 
-export const UpdateEpisodeUseCase: Setup = (episodeRepository, fileStorage, uuid) => async ({ id, animeId, order, file, name, synopsis, secondsLong }) => {
+export const UpdateEpisodeUseCase: Setup = (episodeRepository, fileStorage, uuid, animeRepository) => async ({ id, animeId, order, file, name, synopsis, secondsLong }) => {
   const episode = await episodeRepository.loadById({ id })
   if (!episode) throw new NotFoundError('id')
   if (order) {
@@ -17,5 +18,8 @@ export const UpdateEpisodeUseCase: Setup = (episodeRepository, fileStorage, uuid
     if (episode.videoUrl) await fileStorage.delete({ fileName: episode.videoUrl })
     const key = uuid.generate()
     await fileStorage.upload({ file: file.buffer, fileName: `${key}.${file.mimeType.split('/')[1]}` })
+  }
+  if (animeId) {
+    await animeRepository.checkById({ id: animeId })
   }
 }
