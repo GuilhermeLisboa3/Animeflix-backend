fe6dc32d5ab4582abbfe9985f8bc7dfa09eda5c1
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-04-04 20:53:01 -0300

    test: ensure GetCategoryUseCase return NotFoundError if LoadCategoryById returns undefined

--- tests/domain/usecases/category/get-category.spec.ts
+++ tests/domain/usecases/category/get-category.spec.ts
@@ -2,6 +2,7 @@ import { GetCategoryUseCase, GetCategory } from '@/domain/usecases/category'
 import { LoadCategoryById } from '@/domain/contracts/database/category'
 
 import { mock, MockProxy } from 'jest-mock-extended'
+import { NotFoundError } from '@/domain/errors'
 
 describe('GetCategory', () => {
   let categoryRepository: MockProxy<LoadCategoryById>
@@ -10,6 +11,7 @@ describe('GetCategory', () => {
 
   beforeAll(() => {
     categoryRepository = mock()
+    categoryRepository.loadById.mockResolvedValue({ id: 'any_id', name: 'any_name', position: 1 })
     category = { id: '1' }
   })
 
@@ -22,5 +24,13 @@ describe('GetCategory', () => {
 
     expect(categoryRepository.loadById).toHaveBeenCalledWith({ id: '1' })
     expect(categoryRepository.loadById).toHaveBeenCalledTimes(1)
+  })
+
+  it('should return NotFoundError if LoadCategoryById returns undefined', async () => {
+    categoryRepository.loadById.mockResolvedValueOnce(undefined)
+
+    const promise = sut(category)
+
+    await expect(promise).rejects.toThrow(new NotFoundError('categoryId'))
   })
 })
