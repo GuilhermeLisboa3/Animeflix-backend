052355f1772b48bc1eb89cbe6fc1a88d4586921b
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-04-04 12:41:50 -0300

    test: ensure ListFavoriteUseCase call LoadAnimeById with correct input

--- tests/domain/usecases/favorite/list-favorite.spec.ts
+++ tests/domain/usecases/favorite/list-favorite.spec.ts
@@ -1,5 +1,6 @@
 import { ListFavoriteUseCase, ListFavorite } from '@/domain/usecases/favorite'
 import { CheckAccountById } from '@/domain/contracts/database/account'
+import { LoadAnimeById } from '@/domain/contracts/database/anime'
 import { ListFavoriteRepository } from '@/domain/contracts/database/favorite'
 
 import { MockProxy, mock } from 'jest-mock-extended'
@@ -8,6 +9,7 @@ import { NotFoundError } from '@/domain/errors'
 describe('ListFavoriteUseCase', () => {
   let accountRepository: MockProxy<CheckAccountById>
   let favoriteRepository: MockProxy<ListFavoriteRepository>
+  let animeRepository: MockProxy<LoadAnimeById>
   let makeFavorite: { accountId: string }
   let sut: ListFavorite
 
@@ -16,10 +18,12 @@ describe('ListFavoriteUseCase', () => {
     accountRepository = mock()
     accountRepository.checkById.mockResolvedValue(true)
     favoriteRepository = mock()
+    favoriteRepository.list.mockResolvedValue([1, 2])
+    animeRepository = mock()
   })
 
   beforeEach(() => {
-    sut = ListFavoriteUseCase(accountRepository, favoriteRepository)
+    sut = ListFavoriteUseCase(accountRepository, favoriteRepository, animeRepository)
   })
 
   it('should call CheckAccountById with correct input', async () => {
@@ -42,5 +46,13 @@ describe('ListFavoriteUseCase', () => {
 
     expect(favoriteRepository.list).toHaveBeenCalledWith({ userId: 1 })
     expect(favoriteRepository.list).toHaveBeenCalledTimes(1)
+  })
+
+  it('should call LoadAnimeById with correct input', async () => {
+    await sut(makeFavorite)
+
+    expect(animeRepository.loadById).toHaveBeenCalledWith({ id: '1' })
+    expect(animeRepository.loadById).toHaveBeenCalledWith({ id: '2' })
+    expect(animeRepository.loadById).toHaveBeenCalledTimes(2)
   })
 })
