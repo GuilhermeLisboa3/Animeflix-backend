f7bd01cef61e0405cec358406dc1c0242685139a
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-04-04 12:07:24 -0300

    test: ensure ListFavoriteUseCase call ListFavoriteRepository with correct input

--- tests/domain/usecases/favorite/list-favorite.spec.ts
+++ tests/domain/usecases/favorite/list-favorite.spec.ts
@@ -1,11 +1,13 @@
 import { ListFavoriteUseCase, ListFavorite } from '@/domain/usecases/favorite'
 import { CheckAccountById } from '@/domain/contracts/database/account'
+import { ListFavoriteRepository } from '@/domain/contracts/database/favorite'
 
 import { MockProxy, mock } from 'jest-mock-extended'
 import { NotFoundError } from '@/domain/errors'
 
 describe('ListFavoriteUseCase', () => {
   let accountRepository: MockProxy<CheckAccountById>
+  let favoriteRepository: MockProxy<ListFavoriteRepository>
   let makeFavorite: { accountId: string }
   let sut: ListFavorite
 
@@ -13,10 +15,11 @@ describe('ListFavoriteUseCase', () => {
     makeFavorite = { accountId: '1' }
     accountRepository = mock()
     accountRepository.checkById.mockResolvedValue(true)
+    favoriteRepository = mock()
   })
 
   beforeEach(() => {
-    sut = ListFavoriteUseCase(accountRepository)
+    sut = ListFavoriteUseCase(accountRepository, favoriteRepository)
   })
 
   it('should call CheckAccountById with correct input', async () => {
@@ -32,5 +35,12 @@ describe('ListFavoriteUseCase', () => {
     const promise = sut(makeFavorite)
 
     await expect(promise).rejects.toThrow(new NotFoundError('accountId'))
+  })
+
+  it('should call ListFavoriteRepository with correct input', async () => {
+    await sut(makeFavorite)
+
+    expect(favoriteRepository.list).toHaveBeenCalledWith({ userId: 1 })
+    expect(favoriteRepository.list).toHaveBeenCalledTimes(1)
   })
 })
