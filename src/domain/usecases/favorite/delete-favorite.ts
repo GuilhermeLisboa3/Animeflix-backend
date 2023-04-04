f933fb71d13bf9ca9ac5a3f127ada5cc02da3a84
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-04-03 21:41:26 -0300

    feat: ensure DeleteFavoriteUseCase return NotFoundError if CheckAccountById returns false

--- src/domain/usecases/favorite/delete-favorite.ts
+++ src/domain/usecases/favorite/delete-favorite.ts
@@ -1,9 +1,11 @@
 import { CheckAccountById } from '@/domain/contracts/database/account'
+import { NotFoundError } from '@/domain/errors'
 
 type Setup = (accountRepository: CheckAccountById) => DeleteFavorite
 type Input = { accountId: string, animeId: string}
 export type DeleteFavorite = (input: Input) => Promise<void>
 
 export const DeleteFavoriteUseCase: Setup = (accountRepository) => async ({ accountId, animeId }) => {
-  await accountRepository.checkById({ id: Number(accountId) })
+  const existAccount = await accountRepository.checkById({ id: Number(accountId) })
+  if (!existAccount) throw new NotFoundError('accountId')
 }
