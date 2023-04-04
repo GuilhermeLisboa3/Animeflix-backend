8d916d3426def8a14686f024be41ec299d6800c7
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-04-04 16:04:45 -0300

    feat: ensure DeleteLikeUseCase return NotFoundError if CheckAccountById returns false

--- src/domain/usecases/like/delete-like.ts
+++ src/domain/usecases/like/delete-like.ts
@@ -1,9 +1,11 @@
 import { CheckAccountById } from '@/domain/contracts/database/account'
+import { NotFoundError } from '@/domain/errors'
 
 type Setup = (accountRepository: CheckAccountById) => DeleteLike
 type Input = { accountId: string, animeId: string }
 export type DeleteLike = (input: Input) => Promise<void>
 
 export const DeleteLikeUseCase: Setup = (accountRepository) => async ({ accountId, animeId }) => {
-  await accountRepository.checkById({ id: Number(accountId) })
+  const existAccount = await accountRepository.checkById({ id: Number(accountId) })
+  if (!existAccount) throw new NotFoundError('accountId')
 }
