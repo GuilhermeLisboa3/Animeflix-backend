2fc49a63706347cc8b057e81fceb23620d5d796d
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-04-04 13:13:52 -0300

    feat: ensure FavoriteRepository return list animeId on success

--- src/infra/database/postgres/repositories/favorite.ts
+++ src/infra/database/postgres/repositories/favorite.ts
@@ -1,12 +1,18 @@
-import { CreateFavorite, DeleteFavoriteRepository } from '@/domain/contracts/database/favorite'
+import { CreateFavorite, DeleteFavoriteRepository, ListFavoriteRepository } from '@/domain/contracts/database/favorite'
 import { Favorite } from '@/infra/database/postgres/entities'
 
-export class FavoriteRepository implements CreateFavorite, DeleteFavoriteRepository {
+export class FavoriteRepository implements CreateFavorite, DeleteFavoriteRepository, ListFavoriteRepository {
   async create ({ animeId, userId }: CreateFavorite.Input): Promise<void> {
     await Favorite.create({ animeId, userId })
   }
 
   async delete ({ animeId, userId }: DeleteFavoriteRepository.Input): Promise<void> {
     await Favorite.destroy({ where: { animeId, userId } })
+  }
+
+  async list ({ userId }: ListFavoriteRepository.Input): Promise<ListFavoriteRepository.Output> {
+    const listFavorites = await Favorite.findAll({ where: { userId } })
+    const listAnimeId = listFavorites.map(favorite => favorite.animeId)
+    return listAnimeId.length === 0 ? undefined : listAnimeId
   }
 }
