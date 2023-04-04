7f5198d0e6e24bfb77051c22e5b063e5187bfd91
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-04-04 11:56:36 -0300

    feat: ensure ListFavoriteUseCase return NotFoundError if CheckAccountById returns false

--- src/domain/usecases/favorite/list-favorite.ts
+++ src/domain/usecases/favorite/list-favorite.ts
@@ -1,9 +1,11 @@
 import { CheckAccountById } from '@/domain/contracts/database/account'
+import { NotFoundError } from '@/domain/errors'
 
 type Setup = (accountRepository: CheckAccountById) => ListFavorite
 type Input = { accountId: string }
 export type ListFavorite = (input: Input) => Promise<void>
 
 export const ListFavoriteUseCase: Setup = (accountRepository) => async ({ accountId }) => {
-  await accountRepository.checkById({ id: Number(accountId) })
+  const existAccount = await accountRepository.checkById({ id: Number(accountId) })
+  if (!existAccount) throw new NotFoundError('accountId')
 }
