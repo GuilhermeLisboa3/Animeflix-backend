a591ffd7209035710d42dee29234eace6546e5d4
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-04-04 16:07:56 -0300

    test: ensure DeleteLikeUseCase call CheckAnimeById with correct input

--- tests/domain/usecases/like/delete-like.spec.ts
+++ tests/domain/usecases/like/delete-like.spec.ts
@@ -1,11 +1,13 @@
 import { DeleteLikeUseCase, DeleteLike } from '@/domain/usecases/like'
 import { CheckAccountById } from '@/domain/contracts/database/account'
+import { NotFoundError } from '@/domain/errors'
+import { CheckAnimeById } from '@/domain/contracts/database/anime'
 
 import { MockProxy, mock } from 'jest-mock-extended'
-import { NotFoundError } from '@/domain/errors'
 
 describe('DeleteLikeUseCase', () => {
   let accountRepository: MockProxy<CheckAccountById>
+  let animeRepository: MockProxy<CheckAnimeById>
   let makeFavorite: { accountId: string, animeId: string }
   let sut: DeleteLike
 
@@ -13,10 +15,11 @@ describe('DeleteLikeUseCase', () => {
     makeFavorite = { accountId: '1', animeId: '1' }
     accountRepository = mock()
     accountRepository.checkById.mockResolvedValue(true)
+    animeRepository = mock()
   })
 
   beforeEach(() => {
-    sut = DeleteLikeUseCase(accountRepository)
+    sut = DeleteLikeUseCase(accountRepository, animeRepository)
   })
 
   it('should call CheckAccountById with correct input', async () => {
@@ -32,5 +35,12 @@ describe('DeleteLikeUseCase', () => {
     const promise = sut(makeFavorite)
 
     await expect(promise).rejects.toThrow(new NotFoundError('accountId'))
+  })
+
+  it('should call CheckAnimeById with correct input', async () => {
+    await sut(makeFavorite)
+
+    expect(animeRepository.checkById).toHaveBeenCalledWith({ id: 1 })
+    expect(animeRepository.checkById).toHaveBeenCalledTimes(1)
   })
 })
