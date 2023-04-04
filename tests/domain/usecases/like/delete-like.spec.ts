8e4526a9dfab37bd0b9d3e2e90da8ebdeb7c9e07
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-04-04 16:20:39 -0300

    test: add test throw

--- tests/domain/usecases/like/delete-like.spec.ts
+++ tests/domain/usecases/like/delete-like.spec.ts
@@ -41,6 +41,15 @@ describe('DeleteLikeUseCase', () => {
     await expect(promise).rejects.toThrow(new NotFoundError('accountId'))
   })
 
+  it('should rethrow if CheckAccountById throw', async () => {
+    const error = new Error('infa_error')
+    accountRepository.checkById.mockRejectedValueOnce(error)
+
+    const promise = sut(makeFavorite)
+
+    await expect(promise).rejects.toThrow(error)
+  })
+
   it('should call CheckAnimeById with correct input', async () => {
     await sut(makeFavorite)
 
@@ -56,11 +65,29 @@ describe('DeleteLikeUseCase', () => {
     await expect(promise).rejects.toThrow(new NotFoundError('animeId'))
   })
 
+  it('should rethrow if CheckAnimeById throw', async () => {
+    const error = new Error('infa_error')
+    animeRepository.checkById.mockRejectedValueOnce(error)
+
+    const promise = sut(makeFavorite)
+
+    await expect(promise).rejects.toThrow(error)
+  })
+
   it('should call DeleteLikeRepository with correct input', async () => {
     await sut(makeFavorite)
 
     expect(likeRepository.delete).toHaveBeenCalledWith({ userId: 1, animeId: 1 })
     expect(likeRepository.delete).toHaveBeenCalledTimes(1)
+  })
+
+  it('should rethrow if DeleteLikeRepository throw', async () => {
+    const error = new Error('infa_error')
+    likeRepository.delete.mockRejectedValueOnce(error)
+
+    const promise = sut(makeFavorite)
+
+    await expect(promise).rejects.toThrow(error)
   })
 
   it('should return undefined on success', async () => {
