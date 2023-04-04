1c775c94de97ac43a058f01f015ff2df8f83043c
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-04-03 22:19:09 -0300

    feat: ensure DeleteFavoriteController call DeleteFavorite with correct input

--- src/application/controllers/favorite/delete-favorite.ts
+++ src/application/controllers/favorite/delete-favorite.ts
@@ -1,8 +1,15 @@
 import { Validator, ValidationBuilder as build } from '@/application/validation'
+import { DeleteFavorite } from '@/domain/usecases/favorite'
 
 type HttpRequest = { accountId: string, animeId: string }
 
 export class DeleteFavoriteController {
+  constructor (private readonly deleteFavorite: DeleteFavorite) {}
+
+  async perform ({ accountId, animeId }: HttpRequest): Promise<void> {
+    await this.deleteFavorite({ accountId, animeId })
+  }
+
   buildValidators ({ animeId, accountId }: HttpRequest): Validator[] {
     return [
       ...build.of(accountId, 'accountId').required().build(),
