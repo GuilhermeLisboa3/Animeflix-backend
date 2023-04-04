a6005f3cb6aee750e0f44d87bf94f8f7671bb1c8
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-04-04 16:23:22 -0300

    feat: ensure LikeRepository return undefined on success

--- src/infra/database/postgres/repositories/like.ts
+++ src/infra/database/postgres/repositories/like.ts
@@ -1,8 +1,12 @@
-import { CreateLike } from '@/domain/contracts/database/like'
+import { CreateLike, DeleteLikeRepository } from '@/domain/contracts/database/like'
 import { Like } from '@/infra/database/postgres/entities'
 
-export class LikeRepository implements CreateLike {
+export class LikeRepository implements CreateLike, DeleteLikeRepository {
   async create ({ animeId, userId }: CreateLike.Input): Promise<void> {
     await Like.create({ animeId, userId })
+  }
+
+  async delete ({ animeId, userId }: DeleteLikeRepository.Input): Promise<void> {
+    await Like.destroy({ where: { animeId, userId } })
   }
 }
