2d139053a7bfd48eadaba84c0ae9430a32ed0618
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-04-04 16:36:09 -0300

    test: ensure DeleteLikeController call DeleteLike with correct input

--- tests/application/controllers/like/delete-like.spec.ts
+++ tests/application/controllers/like/delete-like.spec.ts
@@ -4,13 +4,15 @@ import { RequiredField } from '@/application/validation'
 describe('DeleteLikeController', () => {
   let sut: DeleteLikeController
   let makeRequest: { accountId: string, animeId: string }
+  let DeleteLike: jest.Mock
 
   beforeAll(() => {
     makeRequest = { accountId: '1', animeId: '1' }
+    DeleteLike = jest.fn()
   })
 
   beforeEach(() => {
-    sut = new DeleteLikeController()
+    sut = new DeleteLikeController(DeleteLike)
   })
 
   it('should build Validators correctly', async () => {
@@ -20,5 +22,12 @@ describe('DeleteLikeController', () => {
       new RequiredField('1', 'accountId'),
       new RequiredField('1', 'animeId')
     ])
+  })
+
+  it('should call DeleteLike with correct input', async () => {
+    await sut.perform(makeRequest)
+
+    expect(DeleteLike).toHaveBeenCalledWith({ accountId: '1', animeId: '1' })
+    expect(DeleteLike).toHaveBeenCalledTimes(1)
   })
 })
