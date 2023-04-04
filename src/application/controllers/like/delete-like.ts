9235e7c5de85dde0d22973101cc72403aa6e4779
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-04-04 16:43:39 -0300

    feat: ensure DeleteLikeController extends Controller

--- src/application/controllers/like/delete-like.ts
+++ src/application/controllers/like/delete-like.ts
@@ -1,18 +1,19 @@
+import { Controller } from '@/application/controllers'
 import { HttpResponse, noContent } from '@/application/helpers'
 import { Validator, ValidationBuilder as build } from '@/application/validation'
 import { DeleteLike } from '@/domain/usecases/like'
 
 type HttpRequest = { accountId: string, animeId: string }
 
-export class DeleteLikeController {
-  constructor (private readonly deleteLike: DeleteLike) {}
+export class DeleteLikeController extends Controller {
+  constructor (private readonly deleteLike: DeleteLike) { super() }
 
   async perform ({ accountId, animeId }: HttpRequest): Promise<HttpResponse> {
     await this.deleteLike({ accountId, animeId })
     return noContent()
   }
 
-  buildValidators ({ animeId, accountId }: HttpRequest): Validator[] {
+  override buildValidators ({ animeId, accountId }: HttpRequest): Validator[] {
     return [
       ...build.of(accountId, 'accountId').required().build(),
       ...build.of(animeId, 'animeId').required().build()
