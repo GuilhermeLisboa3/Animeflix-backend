3abeed3fa02c80e7b30c0dc2a0941e4eb797380a
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-04-04 11:57:30 -0300

    test: ensure ListFavoriteUseCase return NotFoundError if CheckAccountById returns false

--- tests/domain/usecases/favorite/list-favorite.spec.ts
+++ tests/domain/usecases/favorite/list-favorite.spec.ts
@@ -2,6 +2,7 @@ import { ListFavoriteUseCase, ListFavorite } from '@/domain/usecases/favorite'
 import { CheckAccountById } from '@/domain/contracts/database/account'
 
 import { MockProxy, mock } from 'jest-mock-extended'
+import { NotFoundError } from '@/domain/errors'
 
 describe('ListFavoriteUseCase', () => {
   let accountRepository: MockProxy<CheckAccountById>
@@ -11,6 +12,7 @@ describe('ListFavoriteUseCase', () => {
   beforeAll(() => {
     makeFavorite = { accountId: '1' }
     accountRepository = mock()
+    accountRepository.checkById.mockResolvedValue(true)
   })
 
   beforeEach(() => {
@@ -22,5 +24,13 @@ describe('ListFavoriteUseCase', () => {
 
     expect(accountRepository.checkById).toHaveBeenCalledWith({ id: 1 })
     expect(accountRepository.checkById).toHaveBeenCalledTimes(1)
+  })
+
+  it('should return NotFoundError if CheckAccountById returns false', async () => {
+    accountRepository.checkById.mockResolvedValueOnce(false)
+
+    const promise = sut(makeFavorite)
+
+    await expect(promise).rejects.toThrow(new NotFoundError('accountId'))
   })
 })
