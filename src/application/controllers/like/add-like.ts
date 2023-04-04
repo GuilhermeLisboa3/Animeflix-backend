4e7c90f774a324b1bfb803e5c3dde6c0891f3a7d
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-04-04 15:14:39 -0300

    feat: ensure AddLikeController call AddLike with correct input

--- src/application/controllers/like/add-like.ts
+++ src/application/controllers/like/add-like.ts
@@ -1,8 +1,15 @@
 import { Validator, ValidationBuilder as build } from '@/application/validation'
+import { AddLike } from '@/domain/usecases/like'
 
 type HttpRequest = { accountId: string, animeId: number }
 
 export class AddLikeController {
+  constructor (private readonly addLike: AddLike) {}
+
+  async perform ({ accountId, animeId }: HttpRequest): Promise<void> {
+    await this.addLike({ accountId, animeId })
+  }
+
   buildValidators ({ animeId, accountId }: HttpRequest): Validator[] {
     return [
       ...build.of(accountId, 'accountId').required().build(),
