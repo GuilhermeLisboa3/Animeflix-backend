ea2a53dba34c29b6c84faf8636457ed6cfe09469
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-04-04 13:30:57 -0300

    feat: ensure ListFavoriteController extends Controller

--- src/application/controllers/favorite/list-favorite.ts
+++ src/application/controllers/favorite/list-favorite.ts
@@ -1,18 +1,19 @@
+import { Controller } from '@/application/controllers'
 import { HttpResponse, ok } from '@/application/helpers'
 import { Validator, ValidationBuilder as build } from '@/application/validation'
 import { ListFavorite } from '@/domain/usecases/favorite'
 
 type HttpRequest = { accountId: string }
 
-export class ListFavoriteController {
-  constructor (private readonly listFavorite: ListFavorite) {}
+export class ListFavoriteController extends Controller {
+  constructor (private readonly listFavorite: ListFavorite) { super() }
 
   async perform ({ accountId }: HttpRequest): Promise<HttpResponse> {
     const listFavorite = await this.listFavorite({ accountId })
     return ok(listFavorite)
   }
 
-  buildValidators ({ accountId }: HttpRequest): Validator[] {
+  override buildValidators ({ accountId }: HttpRequest): Validator[] {
     return [
       ...build.of(accountId, 'accountId').required().build()
     ]
