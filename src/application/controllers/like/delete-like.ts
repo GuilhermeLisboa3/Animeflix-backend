685c207efe166d6b46f03ed2c09311b6f89e98eb
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-04-04 16:34:15 -0300

    feat: ensure DeleteLikeController call DeleteLike with correct input

--- src/application/controllers/like/delete-like.ts
+++ src/application/controllers/like/delete-like.ts
@@ -1,8 +1,15 @@
 import { Validator, ValidationBuilder as build } from '@/application/validation'
+import { DeleteLike } from '@/domain/usecases/like'
 
 type HttpRequest = { accountId: string, animeId: string }
 
 export class DeleteLikeController {
+  constructor (private readonly deleteLike: DeleteLike) {}
+
+  async perform ({ accountId, animeId }: HttpRequest): Promise<void> {
+    await this.deleteLike({ accountId, animeId })
+  }
+
   buildValidators ({ animeId, accountId }: HttpRequest): Validator[] {
     return [
       ...build.of(accountId, 'accountId').required().build(),
