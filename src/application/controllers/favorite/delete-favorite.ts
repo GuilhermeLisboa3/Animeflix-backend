f9774a398383edcf781e8a85ba07fcb510a39162
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-04-03 22:28:52 -0300

    feat: ensure DeleteFavoriteController extends Controller

--- src/application/controllers/favorite/delete-favorite.ts
+++ src/application/controllers/favorite/delete-favorite.ts
@@ -1,18 +1,19 @@
+import { Controller } from '@/application/controllers'
 import { HttpResponse, noContent } from '@/application/helpers'
 import { Validator, ValidationBuilder as build } from '@/application/validation'
 import { DeleteFavorite } from '@/domain/usecases/favorite'
 
 type HttpRequest = { accountId: string, animeId: string }
 
-export class DeleteFavoriteController {
-  constructor (private readonly deleteFavorite: DeleteFavorite) {}
+export class DeleteFavoriteController extends Controller {
+  constructor (private readonly deleteFavorite: DeleteFavorite) { super() }
 
   async perform ({ accountId, animeId }: HttpRequest): Promise<HttpResponse> {
     await this.deleteFavorite({ accountId, animeId })
     return noContent()
   }
 
-  buildValidators ({ animeId, accountId }: HttpRequest): Validator[] {
+  override buildValidators ({ animeId, accountId }: HttpRequest): Validator[] {
     return [
       ...build.of(accountId, 'accountId').required().build(),
       ...build.of(animeId, 'animeId').required().build()
