66b2610f5fc32083d47a7d30845afbf59ece4167
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-04-04 15:15:24 -0300

    test: ensure AddLikeController call AddLike with correct input

--- tests/application/controllers/like/add-favorite.spec.ts
+++ tests/application/controllers/like/add-favorite.spec.ts
@@ -4,13 +4,15 @@ import { RequiredField } from '@/application/validation'
 describe('AddLikeController', () => {
   let sut: AddLikeController
   let makeRequest: { accountId: string, animeId: number }
+  let AddLike: jest.Mock
 
   beforeAll(() => {
     makeRequest = { accountId: '1', animeId: 1 }
+    AddLike = jest.fn()
   })
 
   beforeEach(() => {
-    sut = new AddLikeController()
+    sut = new AddLikeController(AddLike)
   })
 
   it('should build Validators correctly', async () => {
@@ -20,5 +22,12 @@ describe('AddLikeController', () => {
       new RequiredField('1', 'accountId'),
       new RequiredField(1, 'animeId')
     ])
+  })
+
+  it('should call AddLike with correct input', async () => {
+    await sut.perform(makeRequest)
+
+    expect(AddLike).toHaveBeenCalledWith({ accountId: '1', animeId: 1 })
+    expect(AddLike).toHaveBeenCalledTimes(1)
   })
 })
