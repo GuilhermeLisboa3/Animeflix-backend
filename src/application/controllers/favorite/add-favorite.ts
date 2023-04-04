605b0619fb41e9f356f1297c538e081b2aa829cd
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-04-03 21:12:10 -0300

    feat: ensure AddFavoriteController call AddFavorite with correct input

--- src/application/controllers/favorite/add-favorite.ts
+++ src/application/controllers/favorite/add-favorite.ts
@@ -1,8 +1,15 @@
 import { Validator, ValidationBuilder as build } from '@/application/validation'
+import { AddFavorite } from '@/domain/usecases/favorite'
 
 type HttpRequest = { accountId: string, animeId: number }
 
 export class AddFavoriteController {
+  constructor (private readonly addFavorite: AddFavorite) {}
+
+  async perform ({ accountId, animeId }: HttpRequest): Promise<void> {
+    await this.addFavorite({ accountId, animeId })
+  }
+
   buildValidators ({ animeId, accountId }: HttpRequest): Validator[] {
     return [
       ...build.of(accountId, 'accountId').required().build(),
