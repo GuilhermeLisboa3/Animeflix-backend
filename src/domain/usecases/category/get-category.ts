c7234540846629c4fa8ecd55e0f692b3cfa50a6f
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-04-04 20:51:52 -0300

    feat: ensure GetCategoryUseCase return NotFoundError if LoadCategoryById returns undefined

--- src/domain/usecases/category/get-category.ts
+++ src/domain/usecases/category/get-category.ts
@@ -1,9 +1,11 @@
 import { LoadCategoryById } from '@/domain/contracts/database/category'
+import { NotFoundError } from '@/domain/errors'
 
 type Setup = (categoryRepository: LoadCategoryById) => GetCategory
 type Input = { id: string }
 export type GetCategory = (input: Input) => Promise<void>
 
 export const GetCategoryUseCase: Setup = (categoryRepository) => async ({ id }) => {
-  await categoryRepository.loadById({ id })
+  const category = await categoryRepository.loadById({ id })
+  if (!category) throw new NotFoundError('categoryId')
 }
