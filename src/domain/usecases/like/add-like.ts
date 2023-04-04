9f7133f411a7edb11fa8a245fa4bfbce306999bd
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-04-04 14:41:05 -0300

    feat: ensure AddLikeUseCase call CheckAnimeById with correct input

--- src/domain/usecases/like/add-like.ts
+++ src/domain/usecases/like/add-like.ts
@@ -1,11 +1,13 @@
 import { CheckAccountById } from '@/domain/contracts/database/account'
+import { CheckAnimeById } from '@/domain/contracts/database/anime'
 import { NotFoundError } from '@/domain/errors'
 
-type Setup = (accountRepository: CheckAccountById) => AddLike
+type Setup = (accountRepository: CheckAccountById, animeRepository: CheckAnimeById) => AddLike
 type Input = { accountId: string, animeId: number}
 export type AddLike = (input: Input) => Promise<void>
 
-export const AddLikeUseCase: Setup = (accountRepository) => async ({ accountId, animeId }) => {
+export const AddLikeUseCase: Setup = (accountRepository, animeRepository) => async ({ accountId, animeId }) => {
   const existAccount = await accountRepository.checkById({ id: Number(accountId) })
   if (!existAccount) throw new NotFoundError('accountId')
+  await animeRepository.checkById({ id: animeId })
 }
