edda70ef11471d3b88a33a4664e6e50868f84952
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-04-03 21:17:21 -0300

    feat: ensure AddFavoriteController extends Controller

--- src/application/controllers/favorite/add-favorite.ts
+++ src/application/controllers/favorite/add-favorite.ts
@@ -1,18 +1,19 @@
+import { Controller } from '@/application/controllers'
 import { HttpResponse, noContent } from '@/application/helpers'
 import { Validator, ValidationBuilder as build } from '@/application/validation'
 import { AddFavorite } from '@/domain/usecases/favorite'
 
 type HttpRequest = { accountId: string, animeId: number }
 
-export class AddFavoriteController {
-  constructor (private readonly addFavorite: AddFavorite) {}
+export class AddFavoriteController extends Controller {
+  constructor (private readonly addFavorite: AddFavorite) { super() }
 
   async perform ({ accountId, animeId }: HttpRequest): Promise<HttpResponse> {
     await this.addFavorite({ accountId, animeId })
     return noContent()
   }
 
-  buildValidators ({ animeId, accountId }: HttpRequest): Validator[] {
+  override buildValidators ({ animeId, accountId }: HttpRequest): Validator[] {
     return [
       ...build.of(accountId, 'accountId').required().build(),
       ...build.of(animeId, 'animeId').required().build()
