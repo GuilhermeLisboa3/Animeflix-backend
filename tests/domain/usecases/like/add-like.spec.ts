3227f1e5401f7a7e2fb174ef51482325b7f61672
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-04-04 14:53:26 -0300

    test: ensure AddLikeUseCase return undefined on success

--- tests/domain/usecases/like/add-like.spec.ts
+++ tests/domain/usecases/like/add-like.spec.ts
@@ -62,4 +62,10 @@ describe('AddLikeUseCase', () => {
     expect(likeRepository.create).toHaveBeenCalledWith({ userId: 1, animeId: 1 })
     expect(likeRepository.create).toHaveBeenCalledTimes(1)
   })
+
+  it('should return undefined on success', async () => {
+    const result = await sut(makeFavorite)
+
+    expect(result).toBeUndefined()
+  })
 })
