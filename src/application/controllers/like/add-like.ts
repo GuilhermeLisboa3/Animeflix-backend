aa22b3915dba88cf7ba949b042271c11af871505
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-04-04 15:16:30 -0300

    feat: ensure AddLikeController return 204 on success

--- src/application/controllers/like/add-like.ts
+++ src/application/controllers/like/add-like.ts
@@ -1,3 +1,4 @@
+import { HttpResponse, noContent } from '@/application/helpers'
 import { Validator, ValidationBuilder as build } from '@/application/validation'
 import { AddLike } from '@/domain/usecases/like'
 
@@ -6,8 +7,9 @@ type HttpRequest = { accountId: string, animeId: number }
 export class AddLikeController {
   constructor (private readonly addLike: AddLike) {}
 
-  async perform ({ accountId, animeId }: HttpRequest): Promise<void> {
+  async perform ({ accountId, animeId }: HttpRequest): Promise<HttpResponse> {
     await this.addLike({ accountId, animeId })
+    return noContent()
   }
 
   buildValidators ({ animeId, accountId }: HttpRequest): Validator[] {
