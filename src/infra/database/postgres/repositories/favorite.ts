88328545b78730dd86e6374fbb3b14571cbf42ee
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-04-03 22:06:57 -0300

    feat: ensure FavoriteRepository return undefined on success

--- src/infra/database/postgres/repositories/favorite.ts
+++ src/infra/database/postgres/repositories/favorite.ts
@@ -1,8 +1,12 @@
-import { CreateFavorite } from '@/domain/contracts/database/favorite'
+import { CreateFavorite, DeleteFavoriteRepository } from '@/domain/contracts/database/favorite'
 import { Favorite } from '@/infra/database/postgres/entities'
 
-export class FavoriteRepository implements CreateFavorite {
+export class FavoriteRepository implements CreateFavorite, DeleteFavoriteRepository {
   async create ({ animeId, userId }: CreateFavorite.Input): Promise<void> {
     await Favorite.create({ animeId, userId })
+  }
+
+  async delete ({ animeId, userId }: DeleteFavoriteRepository.Input): Promise<void> {
+    await Favorite.destroy({ where: { animeId, userId } })
   }
 }
