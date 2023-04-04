72e35e7e5d1687cc9bf02b76e7f5b0f88ac1b314
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-04-03 21:19:50 -0300

    test: ensure AddFavoriteController return 400 if AddWatchTime returns NotFoundError

--- tests/application/controllers/favorite/add-favorite.spec.ts
+++ tests/application/controllers/favorite/add-favorite.spec.ts
@@ -1,6 +1,7 @@
 import { AddFavoriteController } from '@/application/controllers/favorite'
 import { RequiredField } from '@/application/validation'
 import { Controller } from '@/application/controllers'
+import { NotFoundError } from '@/domain/errors'
 
 describe('AddFavoriteController', () => {
   let sut: AddFavoriteController
@@ -34,6 +35,16 @@ describe('AddFavoriteController', () => {
 
     expect(AddFavorite).toHaveBeenCalledWith({ accountId: '1', animeId: 1 })
     expect(AddFavorite).toHaveBeenCalledTimes(1)
+  })
+
+  it('should return 400 if AddWatchTime returns NotFoundError', async () => {
+    AddFavorite.mockRejectedValueOnce(new NotFoundError('id'))
+    const httpResponse = await sut.handle(makeRequest)
+
+    expect(httpResponse).toEqual({
+      statusCode: 400,
+      data: new NotFoundError('id')
+    })
   })
 
   it('should return 204 on success', async () => {
