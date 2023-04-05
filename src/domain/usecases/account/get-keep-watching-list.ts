32f1f7699966f5a9ec078e260350ee6722826536
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-04-05 16:30:31 -0300

    feat: ensure GetKeepWatchingListUseCase call LoadWatchTime with correct input

--- src/domain/usecases/account/get-keep-watching-list.ts
+++ src/domain/usecases/account/get-keep-watching-list.ts
@@ -1,9 +1,9 @@
-import { LoadWatchTimeByUserId } from '@/domain/contracts/database/watch-time'
+import { LoadWatchTimeByUserId, LoadWatchTime } from '@/domain/contracts/database/watch-time'
 import { LoadEpisodeById } from '@/domain/contracts/database/episode'
 import { filterLastEpisodesByAnime } from '@/domain/entities'
 import { LoadAnimeById } from '@/domain/contracts/database/anime'
 
-type Setup = (watchTimeRepository: LoadWatchTimeByUserId, episodeRepository: LoadEpisodeById, animeRepository: LoadAnimeById) => KeepWatchingList
+type Setup = (watchTimeRepository: LoadWatchTimeByUserId & LoadWatchTime, episodeRepository: LoadEpisodeById, animeRepository: LoadAnimeById) => KeepWatchingList
 type Input = { id: string }
 export type KeepWatchingList = (input: Input) => Promise<void>
 
@@ -18,6 +18,7 @@ export const GetKeepWatchingListUseCase: Setup = (watchTimeRepository, episodeRe
     const listLastEpisodesByAnime = filterLastEpisodesByAnime(listEpisodes)
     for (const lastEpisodes of listLastEpisodesByAnime) {
       await animeRepository.loadById({ id: lastEpisodes.id.toString() })
+      await watchTimeRepository.load({ userId: id, episodeId: lastEpisodes.id.toString() })
     }
   }
 }
