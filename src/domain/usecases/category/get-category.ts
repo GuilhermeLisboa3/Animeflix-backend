783378b768898477fc757f11c5c2c5e3eacb4c9d
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-04-04 21:56:31 -0300

    feat: ensure GetCategoryUseCase call return category on success

--- src/domain/usecases/category/get-category.ts
+++ src/domain/usecases/category/get-category.ts
@@ -4,10 +4,12 @@ import { NotFoundError } from '@/domain/errors'
 
 type Setup = (categoryRepository: LoadCategoryById, animeRepository: LoadAnimesByCategoryId) => GetCategory
 type Input = { id: string }
-export type GetCategory = (input: Input) => Promise<void>
+type Output = { id: string, name: string, position: number, animes: Array<{ id: number, name: string, thumbnailUrl: string, synopsis: string }> | [] }
+export type GetCategory = (input: Input) => Promise<Output>
 
 export const GetCategoryUseCase: Setup = (categoryRepository, animeRepository) => async ({ id }) => {
   const category = await categoryRepository.loadById({ id })
   if (!category) throw new NotFoundError('categoryId')
-  await animeRepository.loadByCategoryId({ categoryId: id })
+  const animes = await animeRepository.loadByCategoryId({ categoryId: id })
+  return { id: category.id, name: category.name, position: category.position, animes }
 }
