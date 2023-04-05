43ecd038f81bbeacb67884f9e53cad24c0f66d0a
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-04-05 15:45:10 -0300

    test: ensure GetKeepWatchingListUseCase call LoadAnimeById with correct input

--- tests/domain/usecases/account/get-keep-watching-list.spec.ts
+++ tests/domain/usecases/account/get-keep-watching-list.spec.ts
@@ -4,12 +4,15 @@ import { LoadEpisodeById } from '@/domain/contracts/database/episode'
 import { filterLastEpisodesByAnime } from '@/domain/entities'
 
 import { mock, MockProxy } from 'jest-mock-extended'
+import { LoadAnimeById } from '@/domain/contracts/database/anime'
 
 jest.mock('@/domain/entities/filter-last-episodes-by-anime-id')
 
 describe('GetKeepWatchingListUseCase', () => {
   let watchTimeRepository: MockProxy<LoadWatchTimeByUserId>
   let episodeRepository: MockProxy<LoadEpisodeById>
+  let animeRepository: MockProxy<LoadAnimeById>
+  let filterLastEpisodesByAnimeSpy: jest.Mock
   let sut: KeepWatchingList
   let makeAccount: { id: string }
   const makeEpisode = { name: 'any_name', animeId: 1, synopsis: 'any_synopsis', order: 1, videoUrl: 'any_value', id: 1, secondsLong: 100 }
@@ -19,11 +22,14 @@ describe('GetKeepWatchingListUseCase', () => {
     watchTimeRepository.loadByUserId.mockResolvedValue([1, 2])
     episodeRepository = mock()
     episodeRepository.loadById.mockResolvedValue(makeEpisode)
+    animeRepository = mock()
+    filterLastEpisodesByAnimeSpy = jest.fn().mockImplementation(() => ([makeEpisode]))
+    jest.mocked(filterLastEpisodesByAnime).mockImplementation(filterLastEpisodesByAnimeSpy)
     makeAccount = { id: '1' }
   })
 
   beforeEach(() => {
-    sut = GetKeepWatchingListUseCase(watchTimeRepository, episodeRepository)
+    sut = GetKeepWatchingListUseCase(watchTimeRepository, episodeRepository, animeRepository)
   })
 
   it('should call LoadWatchTimeByUserId with correct input', async () => {
@@ -42,11 +48,16 @@ describe('GetKeepWatchingListUseCase', () => {
   })
 
   it('should call filterLastEpisodesByAnime with correct input', async () => {
-    const filterLastEpisodesByAnimeSpy = jest.fn()
-    jest.mocked(filterLastEpisodesByAnime).mockImplementationOnce(filterLastEpisodesByAnimeSpy)
     await sut(makeAccount)
 
     expect(filterLastEpisodesByAnimeSpy).toHaveBeenCalledWith([makeEpisode, makeEpisode])
     expect(filterLastEpisodesByAnimeSpy).toHaveBeenCalledTimes(1)
+  })
+
+  it('should call LoadAnimeById with correct input', async () => {
+    await sut(makeAccount)
+
+    expect(animeRepository.loadById).toHaveBeenCalledWith({ id: '1' })
+    expect(animeRepository.loadById).toHaveBeenCalledTimes(1)
   })
 })
