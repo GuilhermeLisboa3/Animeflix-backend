afeb26c4c84db449065081658d0169fd63e3ef8a
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-04-05 00:17:14 -0300

    feat: ensure GetAnimeByIdUseCase return anime on success

--- src/domain/usecases/anime/get-anime-by-id.ts
+++ src/domain/usecases/anime/get-anime-by-id.ts
@@ -5,19 +5,20 @@ import { NotFoundError } from '@/domain/errors'
 
 type Setup = (animeRepository: LoadAnimeById, episodeRepository: LoadEpisodeByAnimeId, likeRepository: CheckLike) => GetAnimeById
 type Input = { id: string, accountId: string }
-type Output = { id: number, name: string, thumbnailUrl: string, synopsis: string, episodes: Array<{ id: number, name: string, videoUrl: string, synopsis: string, secondsLong: number, order: number }> }
+type Output = { id: number, name: string, thumbnailUrl: string, synopsis: string, episodes: Array<{ id: number, name: string, videoUrl: string, synopsis: string, secondsLong: number, order: number }>, liked: boolean }
 export type GetAnimeById = (input: Input) => Promise<Output>
 
 export const GetAnimeByIdUseCase: Setup = (animeRepository, episodeRepository, likeRepository) => async ({ id, accountId }) => {
   const anime = await animeRepository.loadById({ id })
   if (!anime) throw new NotFoundError('id')
   const episodes = await episodeRepository.loadByAnimeId({ animeId: id })
-  await likeRepository.check({ userId: Number(accountId), animeId: Number(id) })
+  const liked = await likeRepository.check({ userId: Number(accountId), animeId: Number(id) })
   return {
     id: anime.id,
     name: anime.name,
     thumbnailUrl: anime.thumbnailUrl,
     synopsis: anime.synopsis,
-    episodes
+    episodes,
+    liked
   }
 }
