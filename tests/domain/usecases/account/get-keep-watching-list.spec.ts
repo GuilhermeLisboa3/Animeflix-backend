bdeeb463d17038ab17961d31a4b9059a33126349
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-04-05 15:05:03 -0300

    test: ensure GetKeepWatchingListUseCase call filterLastEpisodesByAnime with correct input

--- tests/domain/usecases/account/get-keep-watching-list.spec.ts
+++ tests/domain/usecases/account/get-keep-watching-list.spec.ts
@@ -1,19 +1,24 @@
 import { GetKeepWatchingListUseCase, KeepWatchingList } from '@/domain/usecases/account'
 import { LoadWatchTimeByUserId } from '@/domain/contracts/database/watch-time'
 import { LoadEpisodeById } from '@/domain/contracts/database/episode'
+import { filterLastEpisodesByAnime } from '@/domain/entities'
 
 import { mock, MockProxy } from 'jest-mock-extended'
+
+jest.mock('@/domain/entities/filter-last-episodes-by-anime-id')
 
 describe('GetKeepWatchingListUseCase', () => {
   let watchTimeRepository: MockProxy<LoadWatchTimeByUserId>
   let episodeRepository: MockProxy<LoadEpisodeById>
   let sut: KeepWatchingList
   let makeAccount: { id: string }
+  const makeEpisode = { name: 'any_name', animeId: 1, synopsis: 'any_synopsis', order: 1, videoUrl: 'any_value', id: 1, secondsLong: 100 }
 
   beforeAll(() => {
     watchTimeRepository = mock()
     watchTimeRepository.loadByUserId.mockResolvedValue([1, 2])
     episodeRepository = mock()
+    episodeRepository.loadById.mockResolvedValue(makeEpisode)
     makeAccount = { id: '1' }
   })
 
@@ -34,5 +39,14 @@ describe('GetKeepWatchingListUseCase', () => {
     expect(episodeRepository.loadById).toHaveBeenCalledWith({ id: '1' })
     expect(episodeRepository.loadById).toHaveBeenCalledWith({ id: '2' })
     expect(episodeRepository.loadById).toHaveBeenCalledTimes(2)
+  })
+
+  it('should call filterLastEpisodesByAnime with correct input', async () => {
+    const filterLastEpisodesByAnimeSpy = jest.fn()
+    jest.mocked(filterLastEpisodesByAnime).mockImplementationOnce(filterLastEpisodesByAnimeSpy)
+    await sut(makeAccount)
+
+    expect(filterLastEpisodesByAnimeSpy).toHaveBeenCalledWith([makeEpisode, makeEpisode])
+    expect(filterLastEpisodesByAnimeSpy).toHaveBeenCalledTimes(1)
   })
 })
