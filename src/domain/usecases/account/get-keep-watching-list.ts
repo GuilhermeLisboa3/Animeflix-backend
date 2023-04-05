17971eada37c8946e62c983522174d30aea0fc1e
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-04-05 14:46:26 -0300

    feat: ensure GetKeepWatchingListUseCase call LoadEpisodeById with correct input

--- src/domain/usecases/account/get-keep-watching-list.ts
+++ src/domain/usecases/account/get-keep-watching-list.ts
@@ -1,9 +1,15 @@
 import { LoadWatchTimeByUserId } from '@/domain/contracts/database/watch-time'
+import { LoadEpisodeById } from '@/domain/contracts/database/episode'
 
-type Setup = (watchTimeRepository: LoadWatchTimeByUserId) => KeepWatchingList
+type Setup = (watchTimeRepository: LoadWatchTimeByUserId, episodeRepository: LoadEpisodeById) => KeepWatchingList
 type Input = { id: string }
 export type KeepWatchingList = (input: Input) => Promise<void>
 
-export const GetKeepWatchingListUseCase: Setup = (watchTimeRepository) => async ({ id }) => {
-  await watchTimeRepository.loadByUserId({ userId: id })
+export const GetKeepWatchingListUseCase: Setup = (watchTimeRepository, episodeRepository) => async ({ id }) => {
+  const listEpisodesId = await watchTimeRepository.loadByUserId({ userId: id })
+  if (listEpisodesId.length > 0) {
+    for (const episodeId of listEpisodesId) {
+      await episodeRepository.loadById({ id: episodeId.toString() })
+    }
+  }
 }
