3c7125f5f692ee765a1f4ebfc33acebe49741bf6
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-04-05 14:48:09 -0300

    test: ensure GetKeepWatchingListUseCase call LoadEpisodeById with correct input

--- tests/domain/usecases/account/get-keep-watching-list.spec.ts
+++ tests/domain/usecases/account/get-keep-watching-list.spec.ts
@@ -1,26 +1,38 @@
 import { GetKeepWatchingListUseCase, KeepWatchingList } from '@/domain/usecases/account'
 import { LoadWatchTimeByUserId } from '@/domain/contracts/database/watch-time'
+import { LoadEpisodeById } from '@/domain/contracts/database/episode'
 
 import { mock, MockProxy } from 'jest-mock-extended'
 
 describe('GetKeepWatchingListUseCase', () => {
   let watchTimeRepository: MockProxy<LoadWatchTimeByUserId>
+  let episodeRepository: MockProxy<LoadEpisodeById>
   let sut: KeepWatchingList
   let makeAccount: { id: string }
 
   beforeAll(() => {
     watchTimeRepository = mock()
+    watchTimeRepository.loadByUserId.mockResolvedValue([1, 2])
+    episodeRepository = mock()
     makeAccount = { id: '1' }
   })
 
   beforeEach(() => {
-    sut = GetKeepWatchingListUseCase(watchTimeRepository)
+    sut = GetKeepWatchingListUseCase(watchTimeRepository, episodeRepository)
   })
 
-  it('should call LoadWatchTimeByUserId with correct email', async () => {
+  it('should call LoadWatchTimeByUserId with correct input', async () => {
     await sut(makeAccount)
 
     expect(watchTimeRepository.loadByUserId).toHaveBeenCalledWith({ userId: '1' })
     expect(watchTimeRepository.loadByUserId).toHaveBeenCalledTimes(1)
+  })
+
+  it('should call LoadEpisodeById with correct input', async () => {
+    await sut(makeAccount)
+
+    expect(episodeRepository.loadById).toHaveBeenCalledWith({ id: '1' })
+    expect(episodeRepository.loadById).toHaveBeenCalledWith({ id: '2' })
+    expect(episodeRepository.loadById).toHaveBeenCalledTimes(2)
   })
 })
