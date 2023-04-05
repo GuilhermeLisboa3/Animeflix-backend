b2fbc2ff39fcfbf32eee5f6eb9bbdf8e31d75732
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-04-04 22:04:10 -0300

    test: ensure GetCategoryController return 400 if GetCategory returns NotFoundError

--- tests/application/controllers/category/get-category.spec.ts
+++ tests/application/controllers/category/get-category.spec.ts
@@ -1,29 +1,41 @@
 import { GetCategoryController } from '@/application/controllers/category'
 import { Controller } from '@/application/controllers'
+import { NotFoundError } from '@/domain/errors'
 
 describe('GetCategoryController', () => {
   let sut: GetCategoryController
   let makeRequest: { id: string }
-  let LoadCategory: jest.Mock
+  let GetCategory: jest.Mock
 
   beforeAll(() => {
     makeRequest = { id: '1' }
-    LoadCategory = jest.fn().mockResolvedValue({ id: '1', name: 'any_category', position: 1 })
+    GetCategory = jest.fn().mockResolvedValue({ id: '1', name: 'any_category', position: 1 })
   })
 
   beforeEach(() => {
-    sut = new GetCategoryController(LoadCategory)
+    sut = new GetCategoryController(GetCategory)
   })
 
   it('should extends Controller', async () => {
     expect(sut).toBeInstanceOf(Controller)
   })
 
-  it('should call LoadCategory with correct input', async () => {
+  it('should call GetCategory with correct input', async () => {
     await sut.perform(makeRequest)
 
-    expect(LoadCategory).toHaveBeenCalledWith(makeRequest)
-    expect(LoadCategory).toHaveBeenCalledTimes(1)
+    expect(GetCategory).toHaveBeenCalledWith(makeRequest)
+    expect(GetCategory).toHaveBeenCalledTimes(1)
+  })
+
+  it('should return 400 if GetCategory returns NotFoundError', async () => {
+    GetCategory.mockRejectedValueOnce(new NotFoundError('category'))
+
+    const httpResponse = await sut.handle(makeRequest)
+
+    expect(httpResponse).toEqual({
+      statusCode: 400,
+      data: new NotFoundError('category')
+    })
   })
 
   it('should return 200 on success', async () => {
