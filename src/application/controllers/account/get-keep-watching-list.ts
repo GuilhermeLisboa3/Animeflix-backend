a5fb1753d443b78202d2e132b8e7fafad86bf12b
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-04-05 21:41:58 -0300

    feat: ensure GetKeepWatchingListController call KeepWatchingList with correct input

--- src/application/controllers/account/get-keep-watching-list.ts
+++ src/application/controllers/account/get-keep-watching-list.ts
@@ -1,8 +1,15 @@
 import { Validator, ValidationBuilder as build } from '@/application/validation'
+import { KeepWatchingList } from '@/domain/usecases/account'
 
 type HttpRequest = { accountId: string }
 
 export class GetKeepWatchingListController {
+  constructor (private readonly keepWatchingList: KeepWatchingList) {}
+
+  async perform ({ accountId }: HttpRequest): Promise<void> {
+    await this.keepWatchingList({ id: accountId })
+  }
+
   buildValidators ({ accountId }: HttpRequest): Validator[] {
     return [
       ...build.of(accountId, 'accountId').required().build()
