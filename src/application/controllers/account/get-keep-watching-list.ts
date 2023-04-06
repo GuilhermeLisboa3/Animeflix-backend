0c30639cea3e37b2b193d3d9331275b8645406ba
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-04-05 21:47:23 -0300

    feat: ensure GetKeepWatchingListController extends Controller

--- src/application/controllers/account/get-keep-watching-list.ts
+++ src/application/controllers/account/get-keep-watching-list.ts
@@ -1,18 +1,19 @@
+import { Controller } from '@/application/controllers'
 import { HttpResponse, ok } from '@/application/helpers'
 import { Validator, ValidationBuilder as build } from '@/application/validation'
 import { KeepWatchingList } from '@/domain/usecases/account'
 
 type HttpRequest = { accountId: string }
 
-export class GetKeepWatchingListController {
-  constructor (private readonly keepWatchingList: KeepWatchingList) {}
+export class GetKeepWatchingListController extends Controller {
+  constructor (private readonly keepWatchingList: KeepWatchingList) { super() }
 
   async perform ({ accountId }: HttpRequest): Promise<HttpResponse> {
     const keepWatchingList = await this.keepWatchingList({ id: accountId })
     return ok(keepWatchingList)
   }
 
-  buildValidators ({ accountId }: HttpRequest): Validator[] {
+  override buildValidators ({ accountId }: HttpRequest): Validator[] {
     return [
       ...build.of(accountId, 'accountId').required().build()
     ]
