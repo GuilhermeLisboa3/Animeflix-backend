f24ba5516010eefe90063f1aa4aa4d63572d4945
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-04-07 10:34:21 -0300

    feat: ensure GetAnimeByIdUseCase call CheckFavorite with correct input

--- src/domain/usecases/anime/get-anime-by-id.ts
+++ src/domain/usecases/anime/get-anime-by-id.ts
@@ -1,18 +1,20 @@
 import { LoadAnimeById } from '@/domain/contracts/database/anime'
 import { LoadEpisodeByAnimeId } from '@/domain/contracts/database/episode'
+import { CheckFavorite } from '@/domain/contracts/database/favorite'
 import { CheckLike } from '@/domain/contracts/database/like'
 import { NotFoundError } from '@/domain/errors'
 
-type Setup = (animeRepository: LoadAnimeById, episodeRepository: LoadEpisodeByAnimeId, likeRepository: CheckLike) => GetAnimeById
+type Setup = (animeRepository: LoadAnimeById, episodeRepository: LoadEpisodeByAnimeId, likeRepository: CheckLike, favoriteRepository: CheckFavorite) => GetAnimeById
 type Input = { id: string, accountId: string }
 type Output = { id: number, name: string, thumbnailUrl: string, synopsis: string, episodes: Array<{ id: number, name: string, videoUrl: string, synopsis: string, secondsLong: number, order: number }>, liked: boolean }
 export type GetAnimeById = (input: Input) => Promise<Output>
 
-export const GetAnimeByIdUseCase: Setup = (animeRepository, episodeRepository, likeRepository) => async ({ id, accountId }) => {
+export const GetAnimeByIdUseCase: Setup = (animeRepository, episodeRepository, likeRepository, favoriteRepository) => async ({ id, accountId }) => {
   const anime = await animeRepository.loadById({ id })
   if (!anime) throw new NotFoundError('id')
   const episodes = await episodeRepository.loadByAnimeId({ animeId: id })
   const liked = await likeRepository.check({ userId: Number(accountId), animeId: Number(id) })
+  await favoriteRepository.check({ userId: accountId, animeId: id })
   return {
     id: anime.id,
     name: anime.name,
--- src/main/factories/domain/usecases/anime/get-anime-by-id.ts
+++ src/main/factories/domain/usecases/anime/get-anime-by-id.ts
@@ -1,6 +1,6 @@
 import { GetAnimeById, GetAnimeByIdUseCase } from '@/domain/usecases/anime'
-import { makeAnimeRepository, makeEpisodeRepository, makeLikeRepository } from '@/main/factories/infra/database/postgres'
+import { makeAnimeRepository, makeEpisodeRepository, makeLikeRepository, makeFavoriteRepository } from '@/main/factories/infra/database/postgres'
 
 export const makeGetAnimeById = (): GetAnimeById => {
-  return GetAnimeByIdUseCase(makeAnimeRepository(), makeEpisodeRepository(), makeLikeRepository())
+  return GetAnimeByIdUseCase(makeAnimeRepository(), makeEpisodeRepository(), makeLikeRepository(), makeFavoriteRepository())
 }
