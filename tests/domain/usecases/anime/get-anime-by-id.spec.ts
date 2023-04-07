8293adc7048ecb0ed3e0c3598828d9240d7c1e75
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-04-07 10:36:49 -0300

    test: ensure GetAnimeByIdUseCase call CheckFavorite with correct input

--- tests/domain/usecases/anime/get-anime-by-id.spec.ts
+++ tests/domain/usecases/anime/get-anime-by-id.spec.ts
@@ -2,6 +2,7 @@ import { GetAnimeByIdUseCase, GetAnimeById } from '@/domain/usecases/anime'
 import { LoadAnimeById } from '@/domain/contracts/database/anime'
 import { LoadEpisodeByAnimeId } from '@/domain/contracts/database/episode'
 import { CheckLike } from '@/domain/contracts/database/like'
+import { CheckFavorite } from '@/domain/contracts/database/favorite'
 
 import { MockProxy, mock } from 'jest-mock-extended'
 import { NotFoundError } from '@/domain/errors'
@@ -10,6 +11,7 @@ describe('GetAnimeById', () => {
   let animeRepository: MockProxy<LoadAnimeById>
   let episodeRepository: MockProxy<LoadEpisodeByAnimeId>
   let likeRepository: MockProxy<CheckLike>
+  let favoriteRepository: MockProxy<CheckFavorite>
   let makeAnime: { id: string, accountId: string }
   let sut: GetAnimeById
 
@@ -21,10 +23,11 @@ describe('GetAnimeById', () => {
     episodeRepository.loadByAnimeId.mockResolvedValue([{ id: 1, name: 'any_name', synopsis: 'any_synopsis', order: 1, videoUrl: 'any_value', secondsLong: 1 }])
     likeRepository = mock()
     likeRepository.check.mockResolvedValue(true)
+    favoriteRepository = mock()
   })
 
   beforeEach(() => {
-    sut = GetAnimeByIdUseCase(animeRepository, episodeRepository, likeRepository)
+    sut = GetAnimeByIdUseCase(animeRepository, episodeRepository, likeRepository, favoriteRepository)
   })
 
   it('should call LoadAnimeById with correct input', async () => {
@@ -72,6 +75,31 @@ describe('GetAnimeById', () => {
 
     expect(likeRepository.check).toHaveBeenCalledWith({ userId: 1, animeId: 1 })
     expect(likeRepository.check).toHaveBeenCalledTimes(1)
+  })
+
+  it('should rethrow if CheckLike throw', async () => {
+    const error = new Error('infa_error')
+    likeRepository.check.mockRejectedValueOnce(error)
+
+    const promise = sut(makeAnime)
+
+    await expect(promise).rejects.toThrow(error)
+  })
+
+  it('should call CheckFavorite with correct input', async () => {
+    await sut(makeAnime)
+
+    expect(favoriteRepository.check).toHaveBeenCalledWith({ userId: '1', animeId: '1' })
+    expect(favoriteRepository.check).toHaveBeenCalledTimes(1)
+  })
+
+  it('should rethrow if CheckFavorite throw', async () => {
+    const error = new Error('infa_error')
+    favoriteRepository.check.mockRejectedValueOnce(error)
+
+    const promise = sut(makeAnime)
+
+    await expect(promise).rejects.toThrow(error)
   })
 
   it('should return anime on success', async () => {
